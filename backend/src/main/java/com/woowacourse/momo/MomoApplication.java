ab83f8fb9fce42e0ac72e0dc6816a012ca22fb8f
Seongwon97 <osw970325@naver.com>
2022-07-01 10:54:47 +0900

    chore: 스프링 프로젝트 초기 환경구축

---
+++ backend/src/main/java/com/woowacourse/momo/MomoApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.momo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/woowacourse/momo/MomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowacourse.momo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
