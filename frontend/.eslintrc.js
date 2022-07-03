dec0c797493459bb8f14290d67ba0f4a9f915fc4
이아현 <lah1203@naver.com>
2022-07-03 17:42:52 +0900

    chore: storybook, emotion 설치
    Co-authored-by: Usage <kyr9389@naver.com>

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,36 +1,24 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['prettier', '@typescript-eslint'],
-  extends: [
-    'react-app',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-  ],
+  extends: ['react-app', 'plugin:prettier/recommended', 'plugin:@typescript-eslint/eslint-recommended', 'plugin:storybook/recommended'],
   rules: {
-    'react/function-component-definition': [
-      'error',
-      {
-        namedComponents: 'function-declaration',
+    'react/function-component-definition': ['error', {
+      namedComponents: 'function-declaration'
+    }],
+    'import/order': ['error', {
+      groups: ['external', 'builtin', 'internal', ['parent', 'sibling']],
+      pathGroups: [{
+        pattern: 'react',
+        group: 'external',
+        position: 'before'
+      }],
+      pathGroupsExcludedImportTypes: ['react'],
+      alphabetize: {
+        order: 'asc',
+        caseInsensitive: true
       },
-    ],
-    'import/order': [
-      'error',
-      {
-        groups: ['external', 'builtin', 'internal', ['parent', 'sibling']],
-        pathGroups: [
-          {
-            pattern: 'react',
-            group: 'external',
-            position: 'before',
-          },
-        ],
-        pathGroupsExcludedImportTypes: ['react'],
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-        'newlines-between': 'always',
-      },
-    ],
-  },
+      'newlines-between': 'always'
+    }]
+  }
 };
