5b29799a02724c68ed0926e947a24b3350b86fbb
이아현 <lah1203@naver.com>
2022-07-04 15:43:25 +0900

    chore: react-query 설치
    Co-authored-by: Usage <kyr9389@naver.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
 import Main from 'pages/Main';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <RecoilRoot>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Main />} />
-        </Routes>
-      </Router>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Main />} />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </RecoilRoot>
   );
 }
