fd9ce5ddbb4341911aec9ab8c53c44d186eed661
이아현 <lah1203@naver.com>
2022-07-04 15:39:32 +0900

    chore: recoil 설치
    Co-authored-by: Usage <kyr9389@naver.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import Main from 'pages/Main';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
-    </Router>
+    <RecoilRoot>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </Router>
+    </RecoilRoot>
   );
 }
 
