81f2e0b0cf7be2b3b4a339a4449ed39ffa1e4265
Seongwon97 <osw970325@naver.com>
2022-07-06 14:24:03 +0900

    feat: 카테고리 Service 목록 조회 기능 구현

---
+++ backend/src/main/java/com/woowacourse/momo/service/dto/CategoryResponse.java
@@ -0,0 +1,15 @@
+package com.woowacourse.momo.service.dto;
+
+import com.woowacourse.momo.domain.category.Category;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class CategoryResponse {
+
+    private final long id;
+    private final String name;
+
+    public static CategoryResponse from(Category category) {
+        return new CategoryResponse(category.getId(), category.getName());
+    }
+}
