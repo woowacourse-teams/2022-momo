89071f6002cdfa6481bcad07a920b323e72c2e0a
이아현 <lah1203@naver.com>
2022-07-07 20:58:44 +0900

    feat: 메인, 모임 상세 화면의 일부 컴포넌트에 데이터를 props로 내려줄 수 있도록 변경
    - 데이터 타입 정의
    
    Co-authored-by: Usage <kyr9389@naver.com>

--- frontend/src/components/Detail/SideBar/Info/index.tsx
+++ frontend/src/components/Detail/SideBar/Info/index.tsx
@@ -2,29 +2,49 @@ import { ReactComponent as ClockSVG } from 'assets/clock.svg';
 import { ReactComponent as LocationSVG } from 'assets/location.svg';
 import CategorySVG from 'components/svg/Category';
 import PersonSVG from 'components/svg/Person';
+import { DetailData } from 'types/data';
 
 import * as S from './index.styled';
 
-function Info() {
+// TODO: 카테고리 가져오기
+const category = (categoryId: number) => '식사';
+
+// TODO: 스케줄 기반 날짜 파싱
+const parsedDate = (schedules: DetailData['schedules']) =>
+  '2022년 7월 8일 오후 6 ~ 10시';
+
+// TODO: 참여 로직
+const join = () => {
+  alert('모임에 참여하였습니다!');
+};
+
+function Info({
+  name,
+  schedules,
+  categoryId,
+  location,
+}: Pick<DetailData, 'name' | 'schedules' | 'categoryId' | 'location'>) {
   return (
     <S.Container>
       <S.Wrapper>
         <ClockSVG width={32} />
-        <S.Text>2022년 7월 8일 오후 6 ~ 10시</S.Text>
+        <S.Text>{parsedDate(schedules)}</S.Text>
       </S.Wrapper>
       <S.Wrapper>
         <LocationSVG width={32} />
-        <S.Text>둘둘치킨 선릉공원점</S.Text>
+        <S.Text>{location}</S.Text>
       </S.Wrapper>
       <S.Wrapper>
         <CategorySVG width={32} />
-        <S.Text>식사</S.Text>
+        <S.Text>{category(categoryId)}</S.Text>
       </S.Wrapper>
       <S.Wrapper>
         <PersonSVG width={32} />
-        <S.Text>4기 이프</S.Text>
+        <S.Text>{name}</S.Text>
       </S.Wrapper>
-      <S.JoinButton type="button">참여하기</S.JoinButton>
+      <S.JoinButton type="button" onClick={join}>
+        참여하기
+      </S.JoinButton>
     </S.Container>
   );
 }
