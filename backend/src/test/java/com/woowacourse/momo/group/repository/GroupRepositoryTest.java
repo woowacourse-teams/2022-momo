dd8647e775b8c7a88ac80e4547e3bf2c64de9c1d
Seongwon97 <osw970325@naver.com>
2022-07-11 01:33:45 +0900

    test: 모임 관련 CRUD 테스트 수정

--- backend/src/test/java/com/woowacourse/momo/group/repository/GroupRepositoryTest.java
+++ backend/src/test/java/com/woowacourse/momo/group/repository/GroupRepositoryTest.java
@@ -1,6 +1,12 @@
 package com.woowacourse.momo.group.repository;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import com.woowacourse.momo.group.domain.Duration;
+import com.woowacourse.momo.group.domain.Group;
+import com.woowacourse.momo.group.domain.Schedule;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -9,15 +15,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import com.woowacourse.momo.group.repository.GroupRepository;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import com.woowacourse.momo.group.domain.Duration;
-import com.woowacourse.momo.group.domain.Group;
-import com.woowacourse.momo.group.domain.Schedule;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
 class GroupRepositoryTest {
@@ -25,92 +24,100 @@ class GroupRepositoryTest {
     @Autowired
     private GroupRepository groupRepository;
 
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+
     @DisplayName("스케쥴이 지정된 모임을 저장한다")
     @Test
     void saveGroupWithSchedules() {
-        LocalDate startDate = LocalDate.parse("2022-07-08", DateTimeFormatter.ISO_LOCAL_DATE);
-        LocalDate endDate = LocalDate.parse("2022-07-08", DateTimeFormatter.ISO_LOCAL_DATE);
-        Duration duration = new Duration(startDate, endDate);
+        List<Schedule> schedules = List.of(Schedule.of("월", "11:00:00", "11:00:00"));
+        Group group = constructGroup(schedules);
 
-        Group group = new Group("momo 회의", 1L, 1L, false, duration, LocalDateTime.now(),
-                List.of(Schedule.of("월", "11:00:00", "11:00:00")), "", "");
+        Group savedGroup = groupRepository.save(group);
+        List<Schedule> savedSchedules = scheduleRepository.findByGroupId(savedGroup.getId());
 
-        Group actual = groupRepository.save(group);
-
-        assertThat(actual.getId()).isNotNull();
-        assertThat(actual).usingRecursiveComparison()
-                .isEqualTo(group);
+        assertThat(savedGroup.getId()).isNotNull();
+        assertAll(
+                () -> assertThat(savedGroup).usingRecursiveComparison()
+                        .isEqualTo(group),
+                () -> assertThat(savedSchedules).usingRecursiveComparison()
+                        .ignoringFields("id")
+                        .isEqualTo(schedules)
+        );
     }
 
     @DisplayName("스케쥴이 지정되지 않은 모임을 저장한다")
     @Test
     void saveGroupWithoutSchedules() {
-        LocalDate startDate = LocalDate.parse("2022-07-08", DateTimeFormatter.ISO_LOCAL_DATE);
-        LocalDate endDate = LocalDate.parse("2022-07-08", DateTimeFormatter.ISO_LOCAL_DATE);
-        Duration duration = new Duration(startDate, endDate);
+        List<Schedule> schedules = Collections.emptyList();
+        Group group = constructGroup(schedules);
 
-        Group group = new Group("momo 회의", 1L, 1L, false, duration, LocalDateTime.now(),
-                Collections.emptyList(), "", "");
+        Group savedGroup = groupRepository.save(group);
+        List<Schedule> savedSchedules = scheduleRepository.findByGroupId(savedGroup.getId());
 
-        Group actual = groupRepository.save(group);
-
-        assertThat(actual.getId()).isNotNull();
-        assertThat(actual).usingRecursiveComparison()
-                .isEqualTo(group);
+        assertThat(savedGroup.getId()).isNotNull();
+        assertAll(
+                () -> assertThat(savedGroup).usingRecursiveComparison()
+                        .isEqualTo(group),
+                () -> assertThat(savedSchedules).isEmpty()
+        );
     }
 
     @DisplayName("식별자를 통해 스케줄을 조회한다")
     @Test
     void findById() {
-        LocalDate startDate = LocalDate.parse("2022-07-08", DateTimeFormatter.ISO_LOCAL_DATE);
-        LocalDate endDate = LocalDate.parse("2022-07-08", DateTimeFormatter.ISO_LOCAL_DATE);
-        Duration duration = new Duration(startDate, endDate);
+        List<Schedule> schedules = List.of(Schedule.of("월", "11:00:00", "11:00:00"),
+                Schedule.of("화", "11:00:00", "11:00:00"));
+        Group group = constructGroup(schedules);
+        Group savedGroup = groupRepository.save(group);
 
-        Group group = new Group("momo 회의", 1L, 1L, false, duration, LocalDateTime.now(),
-                List.of(Schedule.of("월", "11:00:00", "11:00:00")), "", "");
+        Optional<Group> foundGroup = groupRepository.findById(savedGroup.getId());
+        List<Schedule> savedSchedules = scheduleRepository.findByGroupId(savedGroup.getId());
 
-        Group expected = groupRepository.save(group);
-        Optional<Group> actual = groupRepository.findById(expected.getId());
-
-        assertThat(actual).isPresent();
-        assertThat(actual.get()).usingRecursiveComparison()
-                .isEqualTo(expected);
+        assertThat(foundGroup).isPresent();
+        assertAll(
+                () -> assertThat(foundGroup.get()).isEqualTo(savedGroup),
+                () -> assertThat(savedSchedules).usingRecursiveComparison()
+                        .ignoringFields("id")
+                        .isEqualTo(schedules)
+        );
     }
 
     @DisplayName("모임 리스트를 조회한다")
     @Test
     void findAll() {
-        LocalDate startDate = LocalDate.parse("2022-07-08", DateTimeFormatter.ISO_LOCAL_DATE);
-        LocalDate endDate = LocalDate.parse("2022-07-08", DateTimeFormatter.ISO_LOCAL_DATE);
-        Duration duration = new Duration(startDate, endDate);
-
-        Group group1 = new Group("momo 회의", 1L, 1L, false, duration, LocalDateTime.now(),
-                List.of(Schedule.of("월", "11:00:00", "11:00:00")), "", "");
-        Group group2 = new Group("momo 회의", 2L, 1L, false, duration, LocalDateTime.now(),
-                List.of(Schedule.of("월", "11:00:00", "11:00:00")), "", "");
-
-        Group expected1 = groupRepository.save(group1);
-        Group expected2 = groupRepository.save(group2);
+        List<Schedule> schedules = List.of(Schedule.of("월", "11:00:00", "11:00:00"));
+        Group group1 = constructGroup(schedules);
+        Group group2 = constructGroup(schedules);
+        Group savedGroup1 = groupRepository.save(group1);
+        Group savedGroup2 = groupRepository.save(group2);
 
         List<Group> actual = groupRepository.findAll();
-        assertThat(actual).contains(expected1, expected2);
+
+        assertThat(actual).usingRecursiveComparison()
+                .isEqualTo(List.of(savedGroup1, savedGroup2));
     }
 
     @DisplayName("식별자를 통해 모임을 삭제한다")
     @Test
     void deleteById() {
+        List<Schedule> schedules = List.of(Schedule.of("월", "11:00:00", "11:00:00"));
+        Group group = constructGroup(schedules);
+        groupRepository.save(group);
+
+        groupRepository.deleteById(group.getId());
+        groupRepository.flush();
+        Optional<Group> actual = groupRepository.findById(group.getId());
+
+        assertThat(actual).isEmpty();
+    }
+
+    private Group constructGroup(List<Schedule> schedules) {
         LocalDate startDate = LocalDate.parse("2022-07-08", DateTimeFormatter.ISO_LOCAL_DATE);
         LocalDate endDate = LocalDate.parse("2022-07-08", DateTimeFormatter.ISO_LOCAL_DATE);
         Duration duration = new Duration(startDate, endDate);
 
-        Group group = new Group("momo 회의", 1L, 1L, false, duration, LocalDateTime.now(),
-                List.of(Schedule.of("월", "11:00:00", "11:00:00")), "", "");
-        Long groupId = groupRepository.save(group).getId();
-
-        groupRepository.deleteById(groupId);
-        groupRepository.flush();
-        Optional<Group> actual = groupRepository.findById(groupId);
-
-        assertThat(actual).isEmpty();
+        return new Group("momo 회의", 1L, 1L, false, duration, LocalDateTime.now(),
+                schedules, "", "");
     }
 }
