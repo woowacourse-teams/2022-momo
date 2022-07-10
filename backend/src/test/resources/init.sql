101b61bef4e9bd7ddc7d0da6e4bd182b9f307121
sinb57 <sinb57@naver.com>
2022-07-10 18:37:00 +0900

    chore: init.sql 파일의 DB 초기화 방식 변경

--- backend/src/test/resources/init.sql
+++ backend/src/test/resources/init.sql
@@ -1,8 +1,54 @@
-truncate table momo_category;
+drop table if exists momo_category CASCADE;
+drop table if exists momo_group CASCADE;
+drop table if exists momo_member CASCADE;
+drop table if exists momo_schedule CASCADE;
 
-insert into momo_category (name) values
-    ('운동'), ('스터디'), ('한 잔'), ('영화'), ('모각코');
+create table momo_category
+(
+    id   bigint auto_increment,
+    name varchar(30) not null,
+    primary key (id)
+);
 
-truncate table momo_member;
+create table momo_group
+(
+    id         bigint auto_increment,
+    categoryId bigint       not null,
+    deadline   timestamp    not null,
+    description clob not null,
+    endDate    date         not null,
+    startDate  date         not null,
+    hostId     bigint       not null,
+    location   varchar(255) not null,
+    name       varchar(255) not null,
+    regular    boolean      not null,
+    primary key (id)
+);
 
-insert into momo_member (name) values ('momo');
+create table momo_member
+(
+    id   bigint auto_increment,
+    name varchar(30) not null,
+    primary key (id)
+);
+
+create table momo_schedule
+(
+    id             bigint auto_increment,
+    endTime        time not null,
+    reservationDay varchar(255),
+    startTime      time not null,
+    group_id       bigint,
+    primary key (id)
+);
+
+
+INSERT INTO momo_category (name)
+VALUES ('운동'),
+       ('스터디'),
+       ('한 잔'),
+       ('영화'),
+       ('모각코');
+
+INSERT INTO momo_member (name)
+VALUES ('momo');
