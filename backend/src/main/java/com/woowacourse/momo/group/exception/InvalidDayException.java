b13e163c392e3b0c2842efc364af7f555952cf94
Seongwon97 <osw970325@naver.com>
2022-07-11 15:37:17 +0900

    refactor: 불필요한 예외 클래스 제거

--- backend/src/main/java/com/woowacourse/momo/group/exception/InvalidDayException.java
+++
@@ -1,8 +0,0 @@
-package com.woowacourse.momo.group.exception;
-
-public class InvalidDayException extends GroupException {
-
-    public InvalidDayException() {
-        super("존재하지 않는 요일입니다.");
-    }
-}
