036c3d9b7f5a1d49bb05cdc6acaa02739c39a874
yukong <kyb062666@gmail.com>
2022-07-13 14:45:13 +0900

    test: Category Service 테스트 코드 수정

--- backend/src/test/java/com/woowacourse/momo/category/service/CategoryServiceTest.java
+++ backend/src/test/java/com/woowacourse/momo/category/service/CategoryServiceTest.java
@@ -3,13 +3,16 @@ package com.woowacourse.momo.category.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.woowacourse.momo.category.domain.Category;
 import com.woowacourse.momo.category.service.dto.response.CategoryResponse;
+import com.woowacourse.momo.category.service.dto.response.CategoryResponseAssembler;
 
 @SpringBootTest
 class CategoryServiceTest {
@@ -20,8 +23,14 @@ class CategoryServiceTest {
     @DisplayName("카테고리 목록을 조회한다")
     @Test
     void findAll() {
+        List<CategoryResponse> expected = Category.getAll()
+                .stream()
+                .map(CategoryResponseAssembler::categoryResponse)
+                .collect(Collectors.toList());
+
         List<CategoryResponse> actual = categoryService.findAll();
 
-        assertThat(actual).hasSize(9);
+        assertThat(actual).usingRecursiveFieldByFieldElementComparator()
+                .isEqualTo(expected);
     }
 }
