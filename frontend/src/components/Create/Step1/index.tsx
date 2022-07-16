4c0c589d1d09dcb49cfb7badb0d16b0d263a599e
이아현 <lah1203@naver.com>
2022-07-16 18:21:40 +0900

    feat: 모임 생성 Step1
    Co-authored-by: Usage <kyr9389@naver.com>

--- frontend/src/components/Create/Step1/index.tsx
+++ frontend/src/components/Create/Step1/index.tsx
@@ -1,17 +1,58 @@
-import { Container, Heading, Input, LabelContainer } from '../@shared/styled';
+import React, { forwardRef, LegacyRef, memo } from 'react';
 
-function Step1() {
+import { GROUP_RULE } from 'constants/rule';
+import { Group } from 'types/data';
+
+import {
+  Container,
+  ErrorColor,
+  Heading,
+  Input,
+  LabelContainer,
+  Label,
+} from '../@shared/styled';
+
+interface Step1Props {
+  useNameState: () => {
+    name: Group['name'];
+    setName: (name: Group['name']) => void;
+  };
+  pressEnterToNext: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+}
+
+function Step1(
+  { useNameState, pressEnterToNext }: Step1Props,
+  ref: LegacyRef<HTMLDivElement>,
+) {
+  const { name, setName } = useNameState();
+  const changeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
   return (
-    <Container>
+    <Container ref={ref}>
       <Heading>
-        모임의 <span>이름</span>을 알려주세요.
+        모임의 <span>이름</span>을 알려주세요. <ErrorColor>*</ErrorColor>
       </Heading>
       <LabelContainer>
-        <p>이름</p>
-        <Input type="text" placeholder="정체를 밝혀라 @_@" autoFocus />
+        <Label>
+          <p>이름</p>
+          <p>
+            {name.length}/{GROUP_RULE.NAME.MAX_LENGTH}
+          </p>
+        </Label>
+        <Input
+          type="text"
+          value={name}
+          onChange={changeName}
+          onKeyPress={pressEnterToNext}
+          placeholder="정체를 밝혀라 @_@"
+          maxLength={GROUP_RULE.NAME.MAX_LENGTH}
+          autoFocus
+        />
       </LabelContainer>
     </Container>
   );
 }
 
-export default Step1;
+export default memo(forwardRef(Step1));
