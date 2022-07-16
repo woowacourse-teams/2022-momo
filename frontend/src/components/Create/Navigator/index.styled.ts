4919a4feb412a8c388b38b48e1c01b3c8d25f50a
이아현 <lah1203@naver.com>
2022-07-16 18:26:28 +0900

    feat: Navigator 컴포넌트 생성
    Co-authored-by: Usage <kyr9389@naver.com>

---
+++ frontend/src/components/Create/Navigator/index.styled.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+
+  margin-bottom: 5rem;
+
+  button {
+    background: none;
+  }
+`;
+
+export { Container };
