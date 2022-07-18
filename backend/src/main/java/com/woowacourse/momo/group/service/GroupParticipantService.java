5d9df1718e394a10f4ff7927e5997ab7832ca6d7
yukong <kyb062666@gmail.com>
2022-07-18 17:12:04 +0900

    feat: 모임 참여 관련 Service 기능 구현

---
+++ backend/src/main/java/com/woowacourse/momo/group/service/GroupParticipantService.java
@@ -0,0 +1,48 @@
+package com.woowacourse.momo.group.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+import com.woowacourse.momo.group.domain.group.Group;
+import com.woowacourse.momo.group.domain.group.GroupRepository;
+import com.woowacourse.momo.member.domain.Member;
+import com.woowacourse.momo.member.domain.MemberRepository;
+import com.woowacourse.momo.member.dto.response.MemberResponse;
+import com.woowacourse.momo.member.dto.response.MemberResponseAssembler;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class GroupParticipantService {
+
+    private final GroupRepository groupRepository;
+    private final MemberRepository memberRepository;
+
+    public void participate(Long groupId, Long memberId) {
+        Group group = findGroupById(groupId);
+        Member participant = findMemberById(memberId);
+
+        group.participate(participant);
+    }
+
+    public List<MemberResponse> findParticipants(Long groupId) {
+        Group group = findGroupById(groupId);
+        List<Member> participants = group.getParticipants();
+
+        return MemberResponseAssembler.memberResponses(participants);
+    }
+
+    private Group findGroupById(Long groupId) {
+        return groupRepository.findById(groupId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 모임입니다."));
+    }
+
+    private Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+    }
+}
---
+++ backend/src/test/java/com/woowacourse/momo/group/service/GroupParticipantServiceTest.java
@@ -0,0 +1,103 @@
+package com.woowacourse.momo.group.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import static com.woowacourse.momo.group.fixture.GroupFixture._6월_30일_23시_59분;
+import static com.woowacourse.momo.group.fixture.GroupFixture._7월_1일부터_2일까지;
+import static com.woowacourse.momo.group.fixture.ScheduleFixture._7월_1일_10시부터_12시까지;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.woowacourse.momo.category.domain.Category;
+import com.woowacourse.momo.group.domain.group.Group;
+import com.woowacourse.momo.group.domain.group.GroupRepository;
+import com.woowacourse.momo.group.domain.participant.GroupParticipantRepository;
+import com.woowacourse.momo.member.domain.Member;
+import com.woowacourse.momo.member.domain.MemberRepository;
+import com.woowacourse.momo.member.dto.response.MemberResponse;
+
+@Transactional
+@SpringBootTest
+class GroupParticipantServiceTest {
+
+    @Autowired
+    private GroupParticipantService groupParticipantService;
+
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @Autowired
+    private GroupParticipantRepository groupParticipantRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private Member savedMember;
+
+    @BeforeEach
+    void setUp() {
+        savedMember = memberRepository.save(new Member("회원", "password", "momo"));
+    }
+
+    private Group saveGroup() {
+        return groupRepository.save(new Group("모모의 스터디", savedMember.getId(), Category.STUDY,
+                _7월_1일부터_2일까지, _6월_30일_23시_59분, List.of(_7월_1일_10시부터_12시까지.newInstance()), "", ""));
+    }
+
+    @DisplayName("모임에 참여한다")
+    @Test
+    void participate() {
+        Group savedGroup = saveGroup();
+
+        groupParticipantService.participate(savedGroup.getId(), savedMember.getId());
+
+        assertThat(groupParticipantRepository.findAll()).hasSize(1);
+    }
+
+    @DisplayName("존재하지 않는 모임에 참여할 수 없다")
+    @Test
+    void participateNotExistGroup() {
+        assertThatThrownBy(() -> groupParticipantService.participate(0L, savedMember.getId()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하지 않는 모임입니다.");
+    }
+
+    @DisplayName("존재하지 않는 사용자는 모임에 참여할 수 없다")
+    @Test
+    void participateNotExistMember() {
+        Group savedGroup = saveGroup();
+
+        assertThatThrownBy(() -> groupParticipantService.participate(savedGroup.getId(), 0L))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하지 않는 사용자입니다.");
+    }
+
+    @DisplayName("모임의 참여자 목록을 조회한다")
+    @Test
+    void findParticipants() {
+        Group savedGroup = saveGroup();
+        groupParticipantService.participate(savedGroup.getId(), savedMember.getId());
+
+        List<MemberResponse> actual = groupParticipantService.findParticipants(savedGroup.getId());
+
+        assertThat(actual).usingRecursiveFieldByFieldElementComparator()
+                .isEqualTo(List.of(savedMember));
+    }
+
+    @DisplayName("존재하지 않는 모임의 참여자 목록을 조회할 수 없다")
+    @Test
+    void findParticipantsNotExistGroup() {
+        assertThatThrownBy(() -> groupParticipantService.findParticipants(0L))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하지 않는 모임입니다.");
+    }
+}
