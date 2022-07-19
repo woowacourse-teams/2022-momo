ed44e444964edb92a7f231b0b25cfcada93fe7d5
sinb57 <sinb57@naver.com>
2022-07-19 20:32:55 +0900

    test: Category 인수테스트 리팩토링

--- backend/src/test/java/com/woowacourse/momo/category/acceptance/CategoryAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/momo/category/acceptance/CategoryAcceptanceTest.java
@@ -2,6 +2,8 @@ package com.woowacourse.momo.category.acceptance;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import static com.woowacourse.momo.common.acceptance.RestHandler.getRequest;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,14 +12,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-
 import com.woowacourse.momo.category.domain.Category;
 import com.woowacourse.momo.category.service.dto.response.CategoryResponse;
 import com.woowacourse.momo.category.service.dto.response.CategoryResponseAssembler;
 import com.woowacourse.momo.common.acceptance.AcceptanceTest;
-import com.woowacourse.momo.common.acceptance.RestHandler;
 
 class CategoryAcceptanceTest extends AcceptanceTest {
 
@@ -26,15 +24,17 @@ class CategoryAcceptanceTest extends AcceptanceTest {
     @DisplayName("카테고리 목록을 조회하다")
     @Test
     void findCategories() {
-        ExtractableResponse<Response> response = RestHandler.getRequest(BASE_URL).extract();
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        List<CategoryResponse> actual = getRequest(BASE_URL)
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .jsonPath()
+                .getList(".", CategoryResponse.class);
 
         List<CategoryResponse> expected = Arrays.stream(Category.values())
                 .map(CategoryResponseAssembler::categoryResponse)
                 .collect(Collectors.toList());
 
-        List<CategoryResponse> actual = List.of(response.as(CategoryResponse[].class));
-        assertThat(actual).usingRecursiveFieldByFieldElementComparator()
+        assertThat(actual).usingRecursiveComparison()
                 .isEqualTo(expected);
     }
 }
