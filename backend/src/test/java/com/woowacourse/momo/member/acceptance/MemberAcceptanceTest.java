2df69ac6ad0b8b72ed16869763e51c2a8fec1882
sinb57 <sinb57@naver.com>
2022-07-19 20:35:54 +0900

    test: Member 인수테스트 DisplayName 수정

--- backend/src/test/java/com/woowacourse/momo/member/acceptance/MemberAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/momo/member/acceptance/MemberAcceptanceTest.java
@@ -3,6 +3,7 @@ package com.woowacourse.momo.member.acceptance;
 import static org.hamcrest.Matchers.is;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
@@ -13,7 +14,6 @@ import com.woowacourse.momo.common.acceptance.RestHandler;
 import com.woowacourse.momo.member.dto.request.ChangeNameRequest;
 import com.woowacourse.momo.member.dto.request.ChangePasswordRequest;
 
-@SuppressWarnings("NonAsciiCharacters")
 public class MemberAcceptanceTest extends AcceptanceTest {
 
     private static final User USER = User.MOMO;
@@ -27,30 +27,34 @@ public class MemberAcceptanceTest extends AcceptanceTest {
         accessToken = authRestHandler.로그인을_하다(USER);
     }
 
+    @DisplayName("개인정보를 조회하다")
     @Test
-    void 회원_정보_조회_테스트() {
+    void findMyInfo() {
         RestHandler.getRequest(accessToken, "/api/members")
                 .statusCode(HttpStatus.OK.value())
                 .body("email", is(USER.getEmail()))
                 .body("name", is(USER.getName()));
     }
 
+    @DisplayName("비밀번호를 수정하다")
     @Test
-    void 회원_비밀번호_수정() {
+    void updatePassword() {
         ChangePasswordRequest request = new ChangePasswordRequest("newPassword1!");
         RestHandler.patchRequest(accessToken, request, "/api/members/password")
                 .statusCode(HttpStatus.OK.value());
     }
 
+    @DisplayName("이름을 수정하다")
     @Test
-    void 회원_이름_수정() {
+    void updateName() {
         ChangeNameRequest request = new ChangeNameRequest("새로운 이름");
         RestHandler.patchRequest(accessToken, request, "/api/members/name")
                 .statusCode(HttpStatus.OK.value());
     }
 
+    @DisplayName("회원탈퇴를 하다")
     @Test
-    void 회원_탈퇴_테스트() {
+    void delete() {
         RestHandler.deleteRequest(accessToken, "/api/members")
                 .statusCode(HttpStatus.NO_CONTENT.value());
     }
