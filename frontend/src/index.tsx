cc815eb44630a58546666c17adb1056815bc1eb6
이아현 <lah1203@naver.com>
2022-07-20 10:36:14 +0900

    chore: MSW warning option off
    Co-authored-by: Usage <kyr9389@naver.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,9 @@ import App from './App';
 
 if (process.env.NODE_ENV === 'development') {
   const { worker } = require('mocks/browser');
-  worker.start();
+  worker.start({
+    onUnhandledRequest: 'bypass',
+  });
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
