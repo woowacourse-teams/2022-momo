eeeff72cb69f8dbee1089c776e4cb974742d68a3
이프(sinb57) <sinb57@naver.com>
2022-07-20 11:12:45 +0900

    [BE] CORS 접근 허용 (#78)

--- backend/src/main/java/com/woowacourse/momo/auth/config/AuthConfiguration.java
+++ backend/src/main/java/com/woowacourse/momo/auth/config/AuthConfiguration.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,5 +26,10 @@ public class AuthConfiguration implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new TokenResolver(jwtTokenProvider));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
