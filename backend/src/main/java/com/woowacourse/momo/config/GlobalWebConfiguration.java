39b263a912149783974a53fb9e5a45cc7d842558
sinb57 <sinb57@naver.com>
2022-07-21 16:45:42 +0900

    refactor: WebConfig 클래스명 수정

--- backend/src/main/java/com/woowacourse/momo/config/GlobalWebConfiguration.java
+++ backend/src/main/java/com/woowacourse/momo/config/GlobalWebConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class GlobalWebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
