c499b7cd4e2d32edf32de40f23cfb709dfc31fa8
이아현 <lah1203@naver.com>
2022-07-21 19:56:28 +0900

    chore: 로그인, 회원가입에 사용하는 상수 선언
      - API 경로 → path
      - 메시지
    
    Co-authored-by: Usage <kyr9389@naver.com>

--- frontend/src/constants/message.ts
+++ frontend/src/constants/message.ts
@@ -1,6 +1,10 @@
 import { GROUP_RULE } from './rule';
 
 const GUIDE_MESSAGE = {
+  AUTH: {
+    LOGIN_SUCCESS: '로그인에 성공했어요.',
+    SIGNUP_SUCCESS: '회원가입에 성공했어요.',
+  },
   DELETE: {
     CONFIRM_REQUEST: '정말로 삭제하실 건가요? 이 작업은 돌이킬 수 없어요 🥺',
     SUCCESS_REQUEST: '모임을 성공적으로 삭제했어요.',
@@ -8,6 +12,11 @@ const GUIDE_MESSAGE = {
 };
 
 const ERROR_MESSAGE = {
+  AUTH: {
+    FAILURE_LOGIN_REQUEST: '로그인에 실패했어요. 관리자에게 문의해주세요 🙇‍♂️🙇‍♀️',
+    FAILURE_SIGNUP_REQUEST:
+      '회원가입에 실패했어요. 관리자에게 문의해주세요 🙇‍♂️🙇‍♀️',
+  },
   CREATE: {
     NAME: `이름의 글자 수는 ${GROUP_RULE.NAME.MIN_LENGTH}자에서 ${GROUP_RULE.NAME.MAX_LENGTH}자 사이여야 해요.`,
     CATEGORY: '카테고리는 꼭 선택해 주세요.',
@@ -16,11 +25,11 @@ const ERROR_MESSAGE = {
     LOCATION: `장소는 ${GROUP_RULE.LOCATION.MAX_LENGTH}자 이내여야 해요.`,
     DESCRIPTION: `설명은 ${GROUP_RULE.DESCRIPTION.MAX_LENGTH}자 이내여야 해요.`,
     FAILURE_REQUEST:
-      '모임을 생성하는데 에러가 발생했어요. 관리자에게 문의 부탁드립니다 🙇‍♂️🙇‍♀️',
+      '모임을 생성하는데 에러가 발생했어요. 관리자에게 문의해주세요 🙇‍♂️🙇‍♀️',
   },
   DELETE: {
     FAILURE_REQUEST:
-      '모임을 삭제하는데 에러가 발생했어요. 관리자에게 문의 부탁드립니다 🙇‍♂️🙇‍♀️',
+      '모임을 삭제하는데 에러가 발생했어요. 관리자에게 문의해주세요 🙇‍♂️🙇‍♀️',
   },
 };
 
