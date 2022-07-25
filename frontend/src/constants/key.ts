6da7ebbbecad84aecf879ded3f5dd7a8ec67e0c4
이아현 <lah1203@naver.com>
2022-07-25 16:37:25 +0900

    feat: 상수화
      - 키, path
    
    Co-authored-by: Usage <kyr9389@naver.com>

--- frontend/src/constants/key.ts
+++ frontend/src/constants/key.ts
@@ -2,6 +2,7 @@ const QUERY_KEY = {
   CATEGORY: 'category',
   GROUP_SUMMARIES: 'groupSummaries',
   GROUP_DETAILS: 'groupDetails',
+  GROUP_PARTICIPANTS: 'groupParticipants',
 };
 
 export { QUERY_KEY };
