fd96d0693c2b697d3ba26212392ddb2786f0e800
이아현 <lah1203@naver.com>
2022-07-27 15:39:02 +0900

    refactor: dayLength → totalOfDay로 변수 네이밍 변경

--- frontend/src/hooks/useCalendar.ts
+++ frontend/src/hooks/useCalendar.ts
@@ -1,19 +1,20 @@
 const useCalendar = (year: number, month: number) => {
   // 이번 달
-  const dayLength = new Date(year, month, 0).getDate();
-  const dates = [...Array(dayLength)].map((_, idx) => idx + 1);
+  const totalOfDay = new Date(year, month, 0).getDate();
+  const dates = [...Array(totalOfDay)].map((_, idx) => idx + 1);
 
   // 지난 달
   const prevMonth = new Date(year, month - 1, 0);
-  const prevDayLength = prevMonth.getDate();
-  const prevDayLengthToShow = (prevMonth.getDay() + 1) % 7;
-  const prevDates = [...Array(prevDayLengthToShow)]
-    .map((_, idx) => prevDayLength - idx)
+  const prevTotalOfDay = prevMonth.getDate();
+  const prevTotalOfDayToShow = (prevMonth.getDay() + 1) % 7;
+  const prevDates = [...Array(prevTotalOfDayToShow)]
+    .map((_, idx) => prevTotalOfDay - idx)
     .reverse();
 
   // 다음 달
-  const nextDayLengthToShow = new Date(year, month - 1, dayLength).getDay() + 1;
-  const nextDates = [...Array(7 - nextDayLengthToShow)].map(
+  const nextTotalOfDayToShow =
+    new Date(year, month - 1, totalOfDay).getDay() + 1;
+  const nextDates = [...Array(7 - nextTotalOfDayToShow)].map(
     (_, idx) => idx + 1,
   );
 
