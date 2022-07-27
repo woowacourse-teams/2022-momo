9fb8aecad8ce525b93dbfb189366efc4747f9285
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-07-27 16:05:11 +0900

    refactor: 메인 페이지 section 분리
      - 추천 모임 리스트 분리

---
+++ frontend/src/components/RecommendGroups/index.tsx
@@ -0,0 +1,31 @@
+import { useQuery } from 'react-query';
+
+import { getGroups } from 'apis/request/group';
+import Card from 'components/Card';
+import { QUERY_KEY } from 'constants/key';
+
+import * as S from './index.styled';
+
+function RecommendGroups() {
+  const { data, isLoading, isError } = useQuery(
+    QUERY_KEY.GROUP_SUMMARIES,
+    getGroups,
+  );
+
+  if (isLoading) return <h2>잠시만 기다려주세요... 🔎</h2>;
+
+  if (isError) return <h2>에러가 발생했습니다!!!! 👿</h2>;
+
+  return (
+    <>
+      <S.Heading>이런 모임, 어때요?</S.Heading>
+      <S.GroupListBox>
+        {data?.map(group => (
+          <Card group={group} key={group.id} />
+        ))}
+      </S.GroupListBox>
+    </>
+  );
+}
+
+export default RecommendGroups;
