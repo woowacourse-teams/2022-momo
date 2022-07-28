9683738fc346b6e6106c88bea76d299e297549a7
AhhyunLee <57928612+LAH1203@users.noreply.github.com>
2022-07-28 10:23:45 +0900

    [FE] 달력 컴포넌트 구현 (#117)
    * feat: 달력 컴포넌트 생성
    
      - 전달된 연도와 월을 바탕으로 해당 월의 달력만 보여주는 방식
    
    * feat: 달력 커스텀훅 생성
    
      - 전달된 연도와 월을 바탕으로 해당 월에서 보여질 일 수(지난달, 이번달, 다음달)를 계산하여 반환
    
    * feat: 이전, 다음 월로 넘어가는 기능 추가
    
      - props로 해당 함수를 전달받도록 구현
    
    * feat: 달력 사이즈 추가
    
      - medium, large
    
    * feat: 달력에 오늘 날짜를 표시하는 부분 추가
    
    * style: 달력에 토, 일요일 구분 색깔 추가
    
    * style: 각 일에 padding 추가
    
    * refactor: dayLength → totalOfDay로 변수 네이밍 변경
    
    * refactor: useCalendar 변수명 변경
    
      - https://github.com/woowacourse-teams/2022-momo/pull/117#discussion_r930740289

---
+++ frontend/src/components/@shared/Calendar/index.tsx
@@ -0,0 +1,95 @@
+import { useTheme } from '@emotion/react';
+
+import LeftArrow from 'components/svg/LeftArrow';
+import RightArrow from 'components/svg/RightArrow';
+import useCalendar from 'hooks/useCalendar';
+
+import * as S from './index.styled';
+
+const days = ['일', '월', '화', '수', '목', '금', '토'];
+
+const dayClassGenerator = (day: string | number) => {
+  switch (day) {
+    case '토':
+    case 6:
+      return 'sat';
+    case '일':
+    case 0:
+      return 'sun';
+    default:
+      return '';
+  }
+};
+
+interface CalendarProps {
+  year: number;
+  month: number;
+  goToPrevMonth: () => void;
+  goToNextMonth: () => void;
+  today: Date;
+  size: 'medium' | 'large';
+}
+
+function Calendar({
+  year,
+  month,
+  goToPrevMonth,
+  goToNextMonth,
+  today,
+  size,
+}: CalendarProps) {
+  const { dates, prevDates, nextDates } = useCalendar(year, month);
+  const theme = useTheme();
+
+  const isToday = (date: number) =>
+    today.getFullYear() === year &&
+    today.getMonth() === month - 1 &&
+    today.getDate() === date;
+
+  return (
+    <S.Container size={size}>
+      <S.Navigator size={size}>
+        <S.Arrow onClick={goToPrevMonth}>
+          <LeftArrow width={30} color={theme.colors.yellow001} />
+        </S.Arrow>
+        <div>
+          {year}년 {month}월
+        </div>
+        <S.Arrow onClick={goToNextMonth}>
+          <RightArrow width={30} color={theme.colors.yellow001} />
+        </S.Arrow>
+      </S.Navigator>
+      <S.Content>
+        {/* 요일 */}
+        {days.map(day => (
+          <S.DayColor className={dayClassGenerator(day)} key={day}>
+            {day}
+          </S.DayColor>
+        ))}
+        {/* 지난달 */}
+        {prevDates.map(date => (
+          <S.PrevNextDate key={`${month - 1}-${date}`}>{date}</S.PrevNextDate>
+        ))}
+        {/* 이번달 */}
+        {dates.map(date => (
+          <S.Date
+            className={
+              isToday(date)
+                ? 'today'
+                : dayClassGenerator(new Date(year, month - 1, date).getDay())
+            }
+            key={`${month}-${date}`}
+          >
+            {date}
+          </S.Date>
+        ))}
+        {/* 다음달 */}
+        {nextDates.map(date => (
+          <S.PrevNextDate key={`${month + 1}-${date}`}>{date}</S.PrevNextDate>
+        ))}
+      </S.Content>
+    </S.Container>
+  );
+}
+
+export default Calendar;
