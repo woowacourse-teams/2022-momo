e574b95e2ff9dd9972f25a69346979e6621f0b0a
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-07-29 17:01:31 +0900

    feat: UserInfo type, query key 설정

--- frontend/src/constants/key.ts
+++ frontend/src/constants/key.ts
@@ -3,6 +3,7 @@ const QUERY_KEY = {
   GROUP_SUMMARIES: 'groupSummaries',
   GROUP_DETAILS: 'groupDetails',
   GROUP_PARTICIPANTS: 'groupParticipants',
+  USER_INFO: 'userInfo',
 };
 
 export { QUERY_KEY };
