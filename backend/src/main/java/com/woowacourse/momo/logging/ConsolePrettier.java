1fc05cdd23491c1b0a478e0d4bc4d360fb5805e9
yukong <kyb062666@gmail.com>
2022-07-29 22:16:15 +0900

    refactor: 로그 레벨에 따라 출력 색상 변경

--- backend/src/main/java/com/woowacourse/momo/logging/ConsolePrettier.java
+++ backend/src/main/java/com/woowacourse/momo/logging/ConsolePrettier.java
@@ -7,14 +7,24 @@ import lombok.NoArgsConstructor;
 public class ConsolePrettier {
 
     private static final String ANSI_RESET = "\u001B[0m";
-    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BLACK = "\u001B[30m";
     private static final String ANSI_RED = "\u001B[31m";
-
-    public static String yellow(String text) {
-        return ANSI_YELLOW + text + ANSI_RESET;
-    }
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_PURPLE = "\u001B[35m";
+    private static final String ANSI_CYAN = "\u001B[36m";
+    private static final String ANSI_WHITE = "\u001B[37m";
 
     public static String red(String text) {
         return ANSI_RED + text + ANSI_RESET;
+    }
+
+    public static String green(String text) {
+        return ANSI_GREEN + text + ANSI_RESET;
+    }
+
+    public static String yellow(String text) {
+        return ANSI_YELLOW + text + ANSI_RESET;
     }
 }
