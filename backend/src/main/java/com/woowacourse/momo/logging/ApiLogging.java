83be83ac42a0b3c58b18a19e488ce59cedec3a0d
yukong <kyb062666@gmail.com>
2022-07-29 14:10:49 +0900

    refactor: 모든 API 요청에 대한 로그 삭제
    - 불필요한 로그라고 판단하여 삭제하였음

--- backend/src/main/java/com/woowacourse/momo/logging/ApiLogging.java
+++ backend/src/main/java/com/woowacourse/momo/logging/ApiLogging.java
@@ -9,30 +9,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class ApiLogging extends Logging {
 
-    private static final String PREFIX_START = "start - ";
-    private static final String PREFIX_FINISHED = "finished - ";
-
-    @Around("allMethod()")
-    public Object infoLog(ProceedingJoinPoint joinPoint) throws Throwable {
-        info(PREFIX_START, joinPoint);
-        Object result = joinPoint.proceed();
-        info(PREFIX_FINISHED, joinPoint);
-        return result;
-    }
-
     @Around("predictedExceptionMethod()")
     public Object warnLog(ProceedingJoinPoint joinPoint) throws Throwable {
-        warn(PREFIX_START, joinPoint);
         Object result = joinPoint.proceed();
-        warn(PREFIX_FINISHED, joinPoint);
+        warn(joinPoint);
         return result;
     }
 
     @Around("exceptionMethod()")
     public Object errorLog(ProceedingJoinPoint joinPoint) throws Throwable {
-        error(PREFIX_START, joinPoint);
         Object result = joinPoint.proceed();
-        error(PREFIX_FINISHED, joinPoint);
+        error(joinPoint);
         return result;
     }
 }
