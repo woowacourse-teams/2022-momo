fc5a189343932261e8048b49409434c41bc84863
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-07-29 16:10:07 +0900

    feat: grouprepository pageable 추가

--- backend/src/main/java/com/woowacourse/momo/group/domain/group/GroupRepository.java
+++ backend/src/main/java/com/woowacourse/momo/group/domain/group/GroupRepository.java
@@ -1,6 +1,10 @@
 package com.woowacourse.momo.group.domain.group;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
+    Page<Group> findAll(Pageable pageable);
+
 }
