1d2bc58527e3461c7141e80673643ccbfb7ceae5
yukong <kyb062666@gmail.com>
2022-08-01 17:44:06 +0900

    refactor: @EnableAspectJAutoProxy 설정 객체 분리

--- backend/src/main/java/com/woowacourse/momo/MomoApplication.java
+++ backend/src/main/java/com/woowacourse/momo/MomoApplication.java
@@ -2,9 +2,7 @@ package com.woowacourse.momo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@EnableAspectJAutoProxy
 @SpringBootApplication
 public class MomoApplication {
 
