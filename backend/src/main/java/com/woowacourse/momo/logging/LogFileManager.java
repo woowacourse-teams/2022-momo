2172660603e08cab6e98bd710710f1a3de640a62
yukong <kyb062666@gmail.com>
2022-08-02 01:39:08 +0900

    refactor: 파일에 로그가 적히는 시간 추가

--- backend/src/main/java/com/woowacourse/momo/logging/LogFileManager.java
+++ backend/src/main/java/com/woowacourse/momo/logging/LogFileManager.java
@@ -18,6 +18,7 @@ public class LogFileManager {
     private static final SimpleDateFormat YEAR_FOLDER_FORMAT = new SimpleDateFormat("yyyy");
     private static final SimpleDateFormat MONTH_FOLDER_FORMAT = new SimpleDateFormat("MM");
     private static final SimpleDateFormat FILE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+    private static final SimpleDateFormat LOG_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
     private static final String EXTENSION = ".txt";
     private static final String LOG_DIRECTORY_BASE_PATH = "./src/log/";
@@ -25,7 +26,8 @@ public class LogFileManager {
     private static final boolean IS_APPENDED = true;
 
     public static void writeExceptionStackTrace(String exceptionStackTrace) {
-        String filePath = createDirectory();
+        Date today = new Date();
+        String filePath = createDirectory(today);
         File file = new File(filePath);
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, IS_APPENDED))) {
@@ -37,20 +39,19 @@ public class LogFileManager {
     }
 
     public static void writeExceptionMessage(Exception exception) {
-        String filePath = createDirectory();
+        Date today = new Date();
+        String filePath = createDirectory(today);
         File file = new File(filePath);
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, IS_APPENDED))) {
-            writeExceptionMessage(exception, writer);
+            writeExceptionMessage(exception, writer, today);
             writer.newLine();
         } catch (IOException e) {
             throw new LogException("로그 작성에 실패하였습니다");
         }
     }
 
-    private static String createDirectory() {
-        Date today = new Date();
-
+    private static String createDirectory(Date today) {
         File baseDirectory = new File(LOG_DIRECTORY_BASE_PATH);
         File yearDirectory = new File(LOG_DIRECTORY_BASE_PATH + YEAR_FOLDER_FORMAT.format(today));
         File monthDirectory = new File(yearDirectory + "/" + MONTH_FOLDER_FORMAT.format(today) + "/");
@@ -59,7 +60,7 @@ public class LogFileManager {
         createDirectoryOrFile(yearDirectory);
         createDirectoryOrFile(monthDirectory);
 
-        return monthDirectory.getPath() + "/" + getLogFileName();
+        return monthDirectory.getPath() + "/" + getLogFileName(today);
     }
 
     private static void createDirectoryOrFile(File file) {
@@ -68,13 +69,14 @@ public class LogFileManager {
         }
     }
 
-    private static String getLogFileName() {
-        Date today = new Date();
+    private static String getLogFileName(Date today) {
         return FILE_FORMAT.format(today) + EXTENSION;
     }
 
-    private static void writeExceptionMessage(Exception exception, BufferedWriter writer) throws IOException {
-        writer.append(String.valueOf(exception.getClass()))
+    private static void writeExceptionMessage(Exception exception, BufferedWriter writer, Date today) throws IOException {
+        writer.append(LOG_DATE_FORMAT.format(today))
+                .append(" ")
+                .append(String.valueOf(exception.getClass()))
                 .append(": ")
                 .append(exception.getMessage());
         writer.newLine();
