b51214e1e34dc2f998715b14968b2a400013b459
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-08-03 15:29:19 +0900

    feat: 로그인/회원가입 예외처리 추가

--- backend/src/main/java/com/woowacourse/momo/member/domain/MemberRepository.java
+++ backend/src/main/java/com/woowacourse/momo/member/domain/MemberRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByUserIdAndPassword(String userId, String password);
+
+    Member findByUserId(String userId);
 }
