c98a2449bee4cccbb88c13288ce5291742d60a1f
yukong <92148749+kyukong@users.noreply.github.com>
2022-08-05 13:14:36 +0900

    [BE] 로그 파일의 경로 수정 (#203)
    * refactor: 로그 파일 저장 경로 수정
    
    * refactor: 로그 파일 저장 경로를 yml 파일로 관리
    
    * refactor: 조건에 따라 로깅 빈 객체를 등록
    
    * refactor: 모모 어플리케이션에서 사용하는 설정값 이름 수정
    
    * refactor: 중복적인 설정 제거
    
    * refactor: 메서드명 수정

--- backend/src/main/java/com/woowacourse/momo/logging/LogFileManager.java
+++ backend/src/main/java/com/woowacourse/momo/logging/LogFileManager.java
@@ -7,27 +7,24 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
 import com.woowacourse.momo.logging.exception.LogException;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class LogFileManager {
 
-    private static final SimpleDateFormat YEAR_FOLDER_FORMAT = new SimpleDateFormat("yyyy");
-    private static final SimpleDateFormat MONTH_FOLDER_FORMAT = new SimpleDateFormat("MM");
     private static final SimpleDateFormat FILE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
     private static final SimpleDateFormat LOG_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-
     private static final String EXTENSION = ".txt";
-    private static final String LOG_DIRECTORY_BASE_PATH = "./src/log/";
-
     private static final boolean IS_APPENDED = true;
 
-    public static void writeExceptionStackTrace(String exceptionStackTrace) {
+    private final String logPath;
+
+    public LogFileManager(String logPath) {
+        this.logPath = logPath;
+    }
+
+    public void writeExceptionStackTrace(String exceptionStackTrace) {
         Date today = new Date();
-        String filePath = createDirectory(today);
+        String filePath = createFile(today);
         File file = new File(filePath);
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, IS_APPENDED))) {
@@ -38,9 +35,9 @@ public class LogFileManager {
         }
     }
 
-    public static void writeExceptionMessage(Exception exception) {
+    public void writeExceptionMessage(Exception exception) {
         Date today = new Date();
-        String filePath = createDirectory(today);
+        String filePath = createFile(today);
         File file = new File(filePath);
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, IS_APPENDED))) {
@@ -51,29 +48,24 @@ public class LogFileManager {
         }
     }
 
-    private static String createDirectory(Date today) {
-        File baseDirectory = new File(LOG_DIRECTORY_BASE_PATH);
-        File yearDirectory = new File(LOG_DIRECTORY_BASE_PATH + YEAR_FOLDER_FORMAT.format(today));
-        File monthDirectory = new File(yearDirectory + "/" + MONTH_FOLDER_FORMAT.format(today) + "/");
+    private String createFile(Date today) {
+        File baseDirectory = new File(logPath);
+        createDirectory(baseDirectory);
 
-        createDirectoryOrFile(baseDirectory);
-        createDirectoryOrFile(yearDirectory);
-        createDirectoryOrFile(monthDirectory);
-
-        return monthDirectory.getPath() + "/" + getLogFileName(today);
+        return baseDirectory.getPath() + "/" + getLogFileName(today);
     }
 
-    private static void createDirectoryOrFile(File file) {
+    private void createDirectory(File file) {
         if (!file.exists() && !file.mkdir()) {
             throw new LogException("로그 폴더 생성에 실패하였습니다");
         }
     }
 
-    private static String getLogFileName(Date today) {
+    private String getLogFileName(Date today) {
         return FILE_FORMAT.format(today) + EXTENSION;
     }
 
-    private static void writeExceptionMessage(Exception exception, BufferedWriter writer, Date today) throws IOException {
+    private void writeExceptionMessage(Exception exception, BufferedWriter writer, Date today) throws IOException {
         writer.append(LOG_DATE_FORMAT.format(today))
                 .append(" ")
                 .append(String.valueOf(exception.getClass()))
