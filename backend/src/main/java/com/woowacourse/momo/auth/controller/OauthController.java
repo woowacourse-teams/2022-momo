51ca8eff888affab89254c8e2dadbb19908bdd23
sinb57 <sinb57@naver.com>
2022-08-08 12:00:20 +0900

    feat: Google Oauth 로그인 기능 구현

---
+++ backend/src/main/java/com/woowacourse/momo/auth/controller/OauthController.java
@@ -0,0 +1,32 @@
+package com.woowacourse.momo.auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+import com.woowacourse.momo.auth.service.OauthService;
+import com.woowacourse.momo.auth.service.dto.response.LoginResponse;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/auth/oauth2/google")
+@RestController
+public class OauthController {
+
+    private final OauthService oauthService;
+
+    @GetMapping("/login")
+    public ResponseEntity<String> access() {
+        String authUrl = oauthService.generateAuthUrl();
+        return ResponseEntity.ok(authUrl);
+    }
+
+    @GetMapping(value = "/login", params = {"code"})
+    public ResponseEntity<LoginResponse> login(@RequestParam String code) {
+        LoginResponse loginResponse = oauthService.requestAccessToken(code);
+        return ResponseEntity.ok(loginResponse);
+    }
+}
---
+++ backend/src/main/java/com/woowacourse/momo/auth/service/OauthService.java
@@ -0,0 +1,73 @@
+package com.woowacourse.momo.auth.service;
+
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+import com.woowacourse.momo.auth.service.dto.response.LoginResponse;
+import com.woowacourse.momo.auth.support.JwtTokenProvider;
+import com.woowacourse.momo.auth.support.PasswordEncoder;
+import com.woowacourse.momo.auth.support.google.GoogleConnector;
+import com.woowacourse.momo.auth.support.google.GoogleProvider;
+import com.woowacourse.momo.auth.support.google.dto.GoogleUserResponse;
+import com.woowacourse.momo.member.domain.Member;
+import com.woowacourse.momo.member.domain.MemberRepository;
+
+@RequiredArgsConstructor
+@Service
+public class OauthService {
+
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider JwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
+    private final GoogleConnector googleConnector;
+    private final GoogleProvider googleProvider;
+
+    public String generateAuthUrl() {
+        return googleProvider.generateAuthUrl();
+    }
+
+    public LoginResponse requestAccessToken(String code) {
+        GoogleUserResponse response = requestUserInfo(code);
+
+        Long memberId = findOrSaveMember(response);
+        String token = JwtTokenProvider.createToken(memberId);
+        return new LoginResponse(token);
+    }
+
+    private GoogleUserResponse requestUserInfo(String code) {
+        ResponseEntity<GoogleUserResponse> responseEntity = googleConnector.requestUserInfo(code);
+
+        validateResponseStatusOk(responseEntity.getStatusCode());
+
+        return Optional.ofNullable(responseEntity.getBody())
+                .orElseThrow(() -> new IllegalArgumentException("Google Oauth2 Request UserInfo - Non Exist Body"));
+    }
+
+    private void validateResponseStatusOk(HttpStatus status) {
+        if (!status.is2xxSuccessful()) {
+            throw new IllegalArgumentException("Google Oauth2 Request UserInfo - Http Status 2xx Failed");
+        }
+    }
+
+    private Long findOrSaveMember(GoogleUserResponse response) {
+        Member member = memberRepository.findByUserId(response.getEmail());
+        if (member != null) {
+            return member.getId();
+        }
+        return saveMember(response);
+    }
+
+    private Long saveMember(GoogleUserResponse response) {
+        String userId = response.getEmail();
+        String name = response.getName();
+        String password = passwordEncoder.encrypt(googleProvider.getTemporaryPassword());
+
+        Member savedMember = memberRepository.save(new Member(userId, password, name));
+        return savedMember.getId();
+    }
+}
