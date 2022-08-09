4f0f785feb08cb26b2d4e17b64fb0d663b08e28f
이아현 <lah1203@naver.com>
2022-08-09 11:18:07 +0900

    style: 메인 페이지 카테고리 UI 변경

--- frontend/src/components/Category/index.tsx
+++ frontend/src/components/Category/index.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
+
 import useCategory from 'hooks/useCategory';
+import { CategoryType } from 'types/data';
 
 import * as S from './index.styled';
 
 function Category() {
   const { categories } = useCategory();
+  const [selectedCategoryId, setSelectedCategoryId] = useState(-1);
+
+  const selectCategory = (id: CategoryType['id']) => () => {
+    setSelectedCategoryId(id);
+  };
 
   return (
     <S.Box>
       {categories.map(({ id, name }) => (
-        <S.Button type="button" key={id}>
+        <S.Button
+          type="button"
+          onClick={selectCategory(id)}
+          className={selectedCategoryId === id ? 'select' : ''}
+          key={id}
+        >
           <p>{name}</p>
         </S.Button>
       ))}
