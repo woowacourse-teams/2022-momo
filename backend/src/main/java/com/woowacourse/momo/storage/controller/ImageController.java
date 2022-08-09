482667f4be771d087677134e69ff097c4681e7cd
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-08-09 20:31:24 +0900

    feat: file upload/save 컨트롤러 작성

---
+++ backend/src/main/java/com/woowacourse/momo/storage/controller/ImageController.java
@@ -0,0 +1,43 @@
+package com.woowacourse.momo.storage.controller;
+
+import java.net.URI;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+
+import com.woowacourse.momo.storage.service.StorageService;
+
+@RestController
+@RequestMapping("/api/file/")
+@AllArgsConstructor
+public class ImageController {
+
+    private final StorageService storageService;
+
+    @PostMapping
+    public ResponseEntity<Void> imageUpload(@RequestParam("imageFile") MultipartFile imageFile) {
+        String fileName = storageService.save(imageFile);
+
+        return ResponseEntity.created(URI.create("/api/file/" + fileName)).build(); // filename 받아온다면 수정 필요
+    }
+
+    @GetMapping(
+            value = "{imageFileName}",
+            produces = { MediaType.IMAGE_GIF_VALUE, MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE }
+    )
+    public ResponseEntity<byte[]> serveImage(@PathVariable String imageFileName) {
+        byte[] imageBytes = storageService.load(imageFileName);
+
+        return ResponseEntity.ok(imageBytes);
+    }
+
+}
