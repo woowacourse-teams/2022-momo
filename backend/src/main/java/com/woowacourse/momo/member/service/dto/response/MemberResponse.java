f78a067593232a40fbc3ca75d1cbf8a991e0cde1
sinb57 <sinb57@naver.com>
2022-08-09 11:22:31 +0900

    refactor: MemberResponse 빈생성자 어노테이션 추가

--- backend/src/main/java/com/woowacourse/momo/member/service/dto/response/MemberResponse.java
+++ backend/src/main/java/com/woowacourse/momo/member/service/dto/response/MemberResponse.java
@@ -2,8 +2,10 @@ package com.woowacourse.momo.member.service.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class MemberResponse {
 
