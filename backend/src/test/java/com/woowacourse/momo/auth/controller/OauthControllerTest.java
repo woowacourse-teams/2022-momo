f5bdcf84dfa9e53a9f5122a8c9c3752a32136678
이프(sinb57) <sinb57@naver.com>
2022-08-09 14:34:04 +0900

    Revert "[BE] Oauth 기능에 대한 Jenkins 빌드 테스트 오류 해결 (#225)"
    This reverts commit c01b926fb293af081197d73107e99eab63589ea3.

--- backend/src/test/java/com/woowacourse/momo/auth/controller/OauthControllerTest.java
+++ backend/src/test/java/com/woowacourse/momo/auth/controller/OauthControllerTest.java
@@ -18,38 +18,25 @@ import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.woowacourse.momo.auth.service.OauthService;
-import com.woowacourse.momo.auth.service.dto.response.LoginResponse;
-import com.woowacourse.momo.auth.service.dto.response.OauthLinkResponse;
 
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Transactional
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 class OauthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private OauthService oauthService;
-
     @DisplayName("정상적으로 Oauth 인증 링크가 반환되는지 테스트한다")
     @Test
     void access() throws Exception {
-        String oauthLink = "https://accounts.google.com/o/oauth2/auth?scope=openid%20https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile&response_type=code&redirect_uri=http://localhost:8080/api/auth/oauth2/google/login&client_id=clientId";
-        BDDMockito.given(oauthService.generateAuthUrl())
-                .willReturn(new OauthLinkResponse(oauthLink));
-
         mockMvc.perform(get("/api/auth/oauth2/google/login"))
                 .andExpectAll(
                         status().isOk(),
@@ -65,10 +52,6 @@ class OauthControllerTest {
     @DisplayName("정상적으로 로그인될 시 토큰이 발급된다")
     @Test
     void login() throws Exception {
-        String accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjU5OTI3MTMxLCJleHAiOjE2NTk5MzA3MzF9.VG8BIv3X1peT0e16OdSqq4EkkgDd1bHbYX99oglxkS4";
-        BDDMockito.given(oauthService.requestAccessToken(ArgumentMatchers.anyString()))
-                .willReturn(new LoginResponse(accessToken));
-
         String code = "code";
         mockMvc.perform(get("/api/auth/oauth2/google/login")
                         .queryParam("code", code))
