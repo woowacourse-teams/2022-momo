adda79197c90413a95285b92526b33285a4275fb
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-08-11 10:45:51 +0900

    refactor: test restdocs 누락된 어노테이션과 adoc 추가

--- backend/src/test/java/com/woowacourse/momo/storage/controller/ImageControllerTest.java
+++ backend/src/test/java/com/woowacourse/momo/storage/controller/ImageControllerTest.java
@@ -15,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -27,6 +28,7 @@ import com.woowacourse.momo.storage.service.StorageService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 public class ImageControllerTest {
 
     @Autowired
