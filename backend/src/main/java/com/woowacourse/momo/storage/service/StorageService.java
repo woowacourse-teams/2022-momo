e26e1b72258650e30f4e46a01ac07ddfd1775722
sinb57 <sinb57@naver.com>
2022-08-13 03:35:29 +0900

    refactor: 업로드 파일 검증 로직 리팩터링

--- backend/src/main/java/com/woowacourse/momo/storage/service/StorageService.java
+++ backend/src/main/java/com/woowacourse/momo/storage/service/StorageService.java
@@ -1,13 +1,17 @@
 package com.woowacourse.momo.storage.service;
 
+import static org.springframework.http.MediaType.IMAGE_GIF_VALUE;
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,12 +22,13 @@ import com.woowacourse.momo.global.exception.exception.MomoException;
 public class StorageService {
 
     private static final String PATH_PREFIX = "./image-save/";
+    private static final List<String> IMAGE_CONTENT_TYPES = List.of(IMAGE_GIF_VALUE, IMAGE_JPEG_VALUE, IMAGE_PNG_VALUE);
 
     public String save(MultipartFile requestFile) {
+        validateContentType(requestFile);
+
         File savedFile = new File(PATH_PREFIX + requestFile.getOriginalFilename());
         File directory = new File(PATH_PREFIX);
-
-        validateExtension(requestFile);
 
         fileInit(savedFile, directory);
 
@@ -36,14 +41,17 @@ public class StorageService {
         return savedFile.getName();
     }
 
-    private void validateExtension(MultipartFile file) {
+    private void validateContentType(MultipartFile file) {
         String contentType = file.getContentType();
 
-        if (contentType == null || !(contentType.equals(MediaType.IMAGE_GIF_VALUE) ||
-                contentType.equals(MediaType.IMAGE_JPEG_VALUE) ||
-                contentType.equals(MediaType.IMAGE_PNG_VALUE))) {
+        if (contentType == null || isContentTypeNotImage(contentType)) {
             throw new MomoException(ErrorCode.FILE_INVALID_EXTENSION);
         }
+    }
+
+    private boolean isContentTypeNotImage(String contentType) {
+        return IMAGE_CONTENT_TYPES.stream()
+                .noneMatch(contentType::equals);
     }
 
     private void fileInit(File temporary, File directory) {
