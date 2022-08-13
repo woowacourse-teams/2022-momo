cef3a8fc4bb05d60aaba440cc1ae69ce453cdbb1
이아현 <lah1203@naver.com>
2022-08-13 11:48:05 +0900

    feat: 스낵바 컴포넌트 생성
      - 페이지 레이아웃에 스낵바 추가

---
+++ frontend/src/components/@shared/Snackbar/index.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+
+import Portal from 'components/Portal';
+import useClosingState from 'hooks/useClosingState';
+import useSnackbar from 'hooks/useSnackbar';
+
+import * as S from './index.styled';
+
+const snackbarAnimationTime = 500;
+
+interface SnackbarProps {
+  children: string;
+}
+
+function Snackbar({ children }: SnackbarProps) {
+  const { isClosing, close } = useClosingState(snackbarAnimationTime, () => {
+    resetSnackbar();
+  });
+  const { resetSnackbar } = useSnackbar();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      close();
+    }, snackbarAnimationTime * 2);
+
+    return () => clearTimeout(timeout);
+  }, [children, close]);
+
+  return (
+    <Portal to="snackbar">
+      <S.Container
+        animationTime={snackbarAnimationTime}
+        className={isClosing ? 'close' : ''}
+      >
+        {children}
+      </S.Container>
+    </Portal>
+  );
+}
+
+export default Snackbar;
--- frontend/src/layouts/Page/index.tsx
+++ frontend/src/layouts/Page/index.tsx
@@ -1,4 +1,8 @@
+import { useRecoilValue } from 'recoil';
+
+import Snackbar from 'components/@shared/Snackbar';
 import Header from 'components/Header';
+import { snackbarState } from 'store/states';
 
 import * as S from './index.styled';
 
@@ -7,10 +11,13 @@ interface PageProps {
 }
 
 function Page({ children }: PageProps) {
+  const { isShowing, message } = useRecoilValue(snackbarState);
+
   return (
     <S.PageContainer>
       <Header />
       <S.Content>{children}</S.Content>
+      {isShowing && <Snackbar>{message}</Snackbar>}
     </S.PageContainer>
   );
 }
