f9b62438330cfee823e104f17466c87c70e3d1c9
이아현 <lah1203@naver.com>
2022-08-17 16:35:31 +0900

    feat: 전달된 쿼리 바탕으로 URL을 만들어주는 함수 생성

---
+++ frontend/src/utils/url.ts
@@ -0,0 +1,18 @@
+interface QueryParams {
+  [key: string]: unknown;
+}
+
+const makeUrl = (baseUrl: string, queryParams: QueryParams) => {
+  const queries = Object.keys(queryParams)
+    .map(queryKey => {
+      const value = queryParams[queryKey];
+
+      return value === false ? '' : `${queryKey}=${value}`;
+    })
+    .filter(query => query !== '')
+    .join('&');
+
+  return [baseUrl, queries].join('?');
+};
+
+export { makeUrl };
