a03523dc89b549bbf4262720da147dd5f1c41008
이프(sinb57) <sinb57@naver.com>
2022-08-30 14:35:14 +0900

    [BE] 조회 클래스 리팩토링 (#322)
    * refactor: GroupSpecification 패키지 이동
    
    * fix: 종료여부 확인과정에서 잘못된 쿼리생성 수정
    
    * refactor: 매직넘버 제거
    
    * refactor: 패키지 분리
    
    * style: enum 상수 선언 스타일 수정
    
    * style: 무의미한 빈줄 제거

--- backend/src/main/java/com/woowacourse/momo/group/service/GroupFindService.java
+++ backend/src/main/java/com/woowacourse/momo/group/service/GroupFindService.java
@@ -15,8 +15,8 @@ import com.woowacourse.momo.global.exception.exception.ErrorCode;
 import com.woowacourse.momo.global.exception.exception.MomoException;
 import com.woowacourse.momo.group.domain.group.Group;
 import com.woowacourse.momo.group.domain.group.GroupRepository;
-import com.woowacourse.momo.group.domain.group.GroupSpecification;
 import com.woowacourse.momo.group.service.dto.request.GroupFindRequest;
+import com.woowacourse.momo.group.service.specification.GroupSpecification;
 import com.woowacourse.momo.member.domain.Member;
 
 @RequiredArgsConstructor
