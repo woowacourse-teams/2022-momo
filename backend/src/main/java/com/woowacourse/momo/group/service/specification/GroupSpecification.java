58f312975eefba14f727020b35a220c8a609b4aa
sinb57 <sinb57@naver.com>
2022-09-05 02:21:57 +0900

    refactor: GroupSpecification 리팩터링

--- backend/src/main/java/com/woowacourse/momo/group/service/specification/GroupSpecification.java
+++ backend/src/main/java/com/woowacourse/momo/group/service/specification/GroupSpecification.java
@@ -2,6 +2,7 @@ package com.woowacourse.momo.group.service.specification;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import javax.persistence.criteria.Subquery;
@@ -21,27 +22,34 @@ public class GroupSpecification {
         return (root, query, criteriaBuilder) -> criteriaBuilder.conjunction();
     }
 
+    public Specification<Group> filterByHosted(Member member) {
+        return (root, query, criteriaBuilder) -> isHost(member, root, criteriaBuilder);
+    }
+
+    private Predicate isHost(Member member, Root<Group> root, CriteriaBuilder builder) {
+        return builder.equal(GroupAttribute.HOST.from(root), member);
+    }
+
     public Specification<Group> filterByParticipated(Member member) {
         return (root, query, criteriaBuilder) -> {
             query.distinct(true);
 
-            Predicate isHost = criteriaBuilder.equal(GroupAttribute.HOST.from(root), member);
-
-            Subquery<Participant> subQuery = query.subquery(Participant.class);
-            Root<Participant> participant = subQuery.from(Participant.class);
-            subQuery.select(participant.get("member").get("id"))
-                    .where(criteriaBuilder.and(
-                            criteriaBuilder.equal(participant.get("group"), root.get("id")),
-                            criteriaBuilder.equal(participant.get("member"), member))
-                    );
-
-            Predicate isParticipated = criteriaBuilder.exists(subQuery);
-            return criteriaBuilder.or(isHost, isParticipated);
+            Predicate isHost = isHost(member, root, criteriaBuilder);
+            Predicate isParticipant = isParticipant(member, root, query, criteriaBuilder);
+            return criteriaBuilder.or(isHost, isParticipant);
         };
     }
 
-    public Specification<Group> filterByHosted(Member member) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(GroupAttribute.HOST.from(root), member);
+    private Predicate isParticipant(Member member, Root<Group> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+        Subquery<Participant> subQuery = query.subquery(Participant.class);
+        Root<Participant> subRoot = subQuery.from(Participant.class);
+
+        subQuery.select(ParticipantAttribute.MEMBER_ID.from(subRoot))
+                .where(builder.and(
+                        builder.equal(ParticipantAttribute.GROUP_ID.from(subRoot), GroupAttribute.ID.from(root)),
+                        builder.equal(ParticipantAttribute.MEMBER_ID.from(subRoot), member.getId()))
+                );
+        return builder.exists(subQuery);
     }
 
     public Specification<Group> filterByCategory(Long categoryId) {
@@ -57,7 +65,8 @@ public class GroupSpecification {
             return (root, query, criteriaBuilder) -> criteriaBuilder.conjunction();
         }
         return (root, query, criteriaBuilder) -> {
-            Predicate nameContainKeyword = criteriaBuilder.like(GroupAttribute.NAME.from(root), "%" + keyword + "%");
+            Predicate nameContainKeyword = criteriaBuilder.like(
+                    GroupAttribute.NAME.from(root), "%" + keyword + "%");
             Predicate descriptionContainKeyword = criteriaBuilder.like(
                     GroupAttribute.DESCRIPTION.from(root), "%" + keyword + "%");
             return criteriaBuilder.or(nameContainKeyword, descriptionContainKeyword);
@@ -82,13 +91,17 @@ public class GroupSpecification {
         };
     }
 
-    private Predicate isNotParticipantSizeFull(Root<Group> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+    private Predicate isNotParticipantSizeFull(Root<Group> root, CriteriaQuery<?> query,
+                                               CriteriaBuilder criteriaBuilder) {
         Subquery<Long> subQuery = query.subquery(Long.class);
-        Root<Member> member = subQuery.from(Member.class);
-        subQuery.select(criteriaBuilder.count(member.get("id")))
+        Root<Member> subRoot = subQuery.from(Member.class);
+
+        Expression<Long> memberId = MemberAttribute.ID.from(subRoot);
+
+        subQuery.select(criteriaBuilder.count(memberId))
                 .where(criteriaBuilder.or(
-                        isHost(root, member, criteriaBuilder),
-                        isParticipant(root, member, subQuery, criteriaBuilder))
+                        isHost(root, subRoot, criteriaBuilder),
+                        isParticipant(root, memberId, subQuery, criteriaBuilder))
                 );
         return criteriaBuilder.lessThan(subQuery, GroupAttribute.CAPACITY.from(root));
     }
@@ -97,12 +110,13 @@ public class GroupSpecification {
         return criteriaBuilder.equal(GroupAttribute.HOST.from(root), member);
     }
 
-    private Predicate isParticipant(Root<Group> root, Root<Member> member, Subquery<Long> query, CriteriaBuilder criteriaBuilder) {
-        Subquery<Participant> subQuery = query.subquery(Participant.class);
-        Root<Participant> participant = subQuery.from(Participant.class);
-        subQuery.select(participant.get("member").get("id"))
-                .where(criteriaBuilder.equal(participant.get("group"), root));
-        return criteriaBuilder.in(member.get("id")).value(subQuery);
+    private Predicate isParticipant(Root<Group> root, Expression<Long> memberId, Subquery<Long> query,
+                                    CriteriaBuilder builder) {
+        Subquery<Long> subQuery = query.subquery(Long.class);
+        Root<Participant> subRoot = subQuery.from(Participant.class);
+        subQuery.select(ParticipantAttribute.MEMBER_ID.from(subRoot))
+                .where(builder.equal(ParticipantAttribute.GROUP_ID.from(subRoot), GroupAttribute.ID.from(root)));
+        return builder.in(memberId).value(subQuery);
     }
 
     public Specification<Group> orderByDeadline(Boolean orderByDeadline) {
--- backend/src/main/java/com/woowacourse/momo/group/service/specification/MemberAttribute.java
+++ backend/src/main/java/com/woowacourse/momo/group/service/specification/MemberAttribute.java
@@ -6,25 +6,22 @@ import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Root;
 
-import com.woowacourse.momo.group.domain.participant.Participant;
+import com.woowacourse.momo.member.domain.Member;
 
-public enum ParticipantAttribute {
+public enum MemberAttribute {
 
     ID("id"),
-    MEMBER("member"),
-    MEMBER_ID("member", "id"),
-    GROUP_ID("group", "id"),
     ;
 
     private final String startAttribute;
     private final List<String> attributes;
 
-    ParticipantAttribute(String startAttribute, String... attributes) {
+    MemberAttribute(String startAttribute, String... attributes) {
         this.startAttribute = startAttribute;
         this.attributes = List.of(attributes);
     }
 
-    public <T> Expression<T> from(Root<Participant> root) {
+    public <T> Expression<T> from(Root<Member> root) {
         Path<T> path = root.get(startAttribute);
         for (String attribute : attributes) {
             path = path.get(attribute);
