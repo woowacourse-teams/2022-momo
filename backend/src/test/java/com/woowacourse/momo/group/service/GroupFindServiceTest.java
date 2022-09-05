77b3cb5589786fd361f29e765f6353d330583803
sinb57 <sinb57@naver.com>
2022-09-05 16:12:35 +0900

    test: GroupFindServiceTest 리팩토링

--- backend/src/test/java/com/woowacourse/momo/group/service/GroupFindServiceTest.java
+++ backend/src/test/java/com/woowacourse/momo/group/service/GroupFindServiceTest.java
@@ -2,7 +2,10 @@ package com.woowacourse.momo.group.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import static com.woowacourse.momo.category.domain.Category.STUDY;
 import static com.woowacourse.momo.fixture.GroupFixture.MOMO_STUDY;
+import static com.woowacourse.momo.fixture.MemberFixture.DUDU;
+import static com.woowacourse.momo.fixture.MemberFixture.MOMO;
 import static com.woowacourse.momo.fixture.calendar.DeadlineFixture.내일_23시_59분까지;
 import static com.woowacourse.momo.fixture.calendar.DeadlineFixture.이틀후_23시_59분까지;
 import static com.woowacourse.momo.fixture.calendar.DeadlineFixture.일주일후_23시_59분까지;
@@ -14,11 +17,12 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.woowacourse.momo.auth.support.SHA256Encoder;
+import lombok.RequiredArgsConstructor;
+
 import com.woowacourse.momo.category.domain.Category;
 import com.woowacourse.momo.fixture.GroupFixture;
 import com.woowacourse.momo.fixture.calendar.DeadlineFixture;
@@ -27,25 +31,18 @@ import com.woowacourse.momo.group.domain.GroupRepository;
 import com.woowacourse.momo.group.service.request.GroupFindRequest;
 import com.woowacourse.momo.member.domain.Member;
 import com.woowacourse.momo.member.domain.MemberRepository;
-import com.woowacourse.momo.member.domain.Password;
-import com.woowacourse.momo.member.domain.UserId;
 
 @Transactional
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@RequiredArgsConstructor
 @SpringBootTest
 class GroupFindServiceTest {
 
-    @Autowired
-    private GroupFindService groupFindService;
+    private final GroupFindService groupFindService;
+    private final GroupRepository groupRepository;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    private GroupRepository groupRepository;
-
-    @Autowired
-    private MemberRepository memberRepository;
-
-    private Password password;
-    private Member momo;
-    private Member dudu;
+    private Member host;
     private Group group1;
     private Group group2;
     private Group group3;
@@ -55,19 +52,30 @@ class GroupFindServiceTest {
 
     @BeforeEach
     void setUp() throws IllegalAccessException {
-        password = Password.encrypt("momo123!", new SHA256Encoder());
-        momo = memberRepository.save(new Member(UserId.momo("momo"), password, "momo"));
-        dudu = memberRepository.save(new Member(UserId.momo("dudu"), password, "dudu"));
-        group1 = groupRepository.save(constructGroup("모모의 스터디", momo, Category.STUDY, 5, 이틀후_23시_59분까지));
-        group2 = groupRepository.save(constructGroup("모모의 술파티", momo, Category.DRINK, 15, 내일_23시_59분까지));
-        group3 = groupRepository.save(constructGroup("모모의 헬스클럽", momo, Category.HEALTH, 1, 일주일후_23시_59분까지));
-        group4 = groupRepository.save(constructGroup("두두의 스터디", dudu, Category.STUDY, 6, 내일_23시_59분까지));
-        group5 = groupRepository.save(constructGroup("두두의 스터디", dudu, Category.STUDY, 10, 이틀후_23시_59분까지));
-        group5.participate(momo);
+        host = memberRepository.save(MOMO.toMember());
+        group1 = groupRepository.save(constructGroup("모모의 스터디", host, STUDY, 5, 이틀후_23시_59분까지));
+        group2 = groupRepository.save(constructGroup("모모의 술파티", host, Category.DRINK, 15, 내일_23시_59분까지));
+        group3 = groupRepository.save(constructGroup("모모의 헬스클럽", host, Category.HEALTH, 1, 일주일후_23시_59분까지));
 
-        Group group = constructGroup("두두의 스터디", dudu, Category.STUDY, 10, 이틀후_23시_59분까지);
+        Member anotherHost = memberRepository.save(DUDU.toMember());
+        group4 = groupRepository.save(constructGroup("두두의 스터디", anotherHost, STUDY, 6, 내일_23시_59분까지));
+        group5 = groupRepository.save(constructGroup("두두의 스터디", anotherHost, STUDY, 10, 이틀후_23시_59분까지));
+        group5.participate(host);
+
+        Group group = constructGroup("두두의 스터디", anotherHost, STUDY, 10, 이틀후_23시_59분까지);
         GroupFixture.setDeadlinePast(group, 1);
         group6 = groupRepository.save(group);
+    }
+
+    private Group constructGroup(String name, Member host, Category category, int capacity, DeadlineFixture deadline) {
+        return MOMO_STUDY.builder()
+                .name(name)
+                .category(category)
+                .capacity(capacity)
+                .deadline(deadline)
+                .duration(일주일후_하루동안)
+                .schedules(일주일후_10시부터_12시까지)
+                .toGroup(host);
     }
 
     @DisplayName("모임을 조회한다")
@@ -83,6 +91,7 @@ class GroupFindServiceTest {
     @Test
     void findGroups() {
         GroupFindRequest request = new GroupFindRequest();
+
         List<Group> actual = groupFindService.findGroups(request).getContent();
 
         assertThat(actual).usingRecursiveComparison()
@@ -93,7 +102,8 @@ class GroupFindServiceTest {
     @Test
     void findParticipatedGroups() {
         GroupFindRequest request = new GroupFindRequest();
-        List<Group> actual = groupFindService.findParticipatedGroups(request, momo).getContent();
+
+        List<Group> actual = groupFindService.findParticipatedGroups(request, host).getContent();
 
         assertThat(actual).usingRecursiveComparison()
                 .isEqualTo(List.of(group5, group3, group2, group1));
@@ -103,7 +113,8 @@ class GroupFindServiceTest {
     @Test
     void findHostedGroups() {
         GroupFindRequest request = new GroupFindRequest();
-        List<Group> actual = groupFindService.findHostedGroups(request, momo).getContent();
+
+        List<Group> actual = groupFindService.findHostedGroups(request, host).getContent();
 
         assertThat(actual).usingRecursiveComparison()
                 .isEqualTo(List.of(group3, group2, group1));
@@ -112,8 +123,10 @@ class GroupFindServiceTest {
     @DisplayName("카테고리에 해당하는 모임 목록을 조회한다")
     @Test
     void findGroupThatFilterByCategory() {
-        GroupFindRequest request = new GroupFindRequest();
-        request.setCategory(Category.STUDY.getId());
+        GroupFindRequest request = new FindRequestBuilder()
+                .category(STUDY)
+                .build();
+
         List<Group> actual = groupFindService.findGroups(request).getContent();
 
         assertThat(actual).usingRecursiveComparison()
@@ -123,9 +136,10 @@ class GroupFindServiceTest {
     @DisplayName("키워드가 포함된 모임 목록을 조회한다")
     @Test
     void findGroupThatContainKeywords() {
-        String keyword = "모모";
-        GroupFindRequest request = new GroupFindRequest();
-        request.setKeyword(keyword);
+        GroupFindRequest request = new FindRequestBuilder()
+                .keyword("모모")
+                .build();
+
         List<Group> actual = groupFindService.findGroups(request).getContent();
 
         assertThat(actual).usingRecursiveComparison()
@@ -135,8 +149,10 @@ class GroupFindServiceTest {
     @DisplayName("모집 완료된 모임을 제외한 목록을 조회한다")
     @Test
     void findGroupThatExcludeFinishedRecruitment() {
-        GroupFindRequest request = new GroupFindRequest();
-        request.setExcludeFinished(true);
+        GroupFindRequest request = new FindRequestBuilder()
+                .excludeFinished(true)
+                .build();
+
         List<Group> actual = groupFindService.findGroups(request).getContent();
 
         assertThat(actual).usingRecursiveComparison()
@@ -146,8 +162,10 @@ class GroupFindServiceTest {
     @DisplayName("마감기한이 적은 순으로 목록을 조회한다")
     @Test
     void findGroupThatOrderByDeadline() {
-        GroupFindRequest request = new GroupFindRequest();
-        request.setOrderByDeadline(true);
+        GroupFindRequest request = new FindRequestBuilder()
+                .orderByDeadline(true)
+                .build();
+
         List<Group> actual = groupFindService.findGroups(request).getContent();
 
         assertThat(actual).usingRecursiveComparison()
@@ -157,10 +175,11 @@ class GroupFindServiceTest {
     @DisplayName("키워드가 포함된 목록 중 모집 완료된 모임을 제외한 목록을 조회한다")
     @Test
     void findGroupThatContainKeywordsAndExcludeFinishedRecruitment() {
-        String keyword = "모모";
-        GroupFindRequest request = new GroupFindRequest();
-        request.setKeyword(keyword);
-        request.setExcludeFinished(true);
+        GroupFindRequest request = new FindRequestBuilder()
+                .keyword("모모")
+                .excludeFinished(true)
+                .build();
+
         List<Group> actual = groupFindService.findGroups(request).getContent();
 
         assertThat(actual).usingRecursiveComparison()
@@ -170,10 +189,11 @@ class GroupFindServiceTest {
     @DisplayName("키워드가 포함된 목록 중 마감기한이 적게 남은 순으로 목록을 조회한다")
     @Test
     void findGroupThatContainKeywordsOrderByDeadline() {
-        String keyword = "모모";
-        GroupFindRequest request = new GroupFindRequest();
-        request.setKeyword(keyword);
-        request.setOrderByDeadline(true);
+        GroupFindRequest request = new FindRequestBuilder()
+                .keyword("모모")
+                .orderByDeadline(true)
+                .build();
+
         List<Group> actual = groupFindService.findGroups(request).getContent();
 
         assertThat(actual).usingRecursiveComparison()
@@ -183,9 +203,11 @@ class GroupFindServiceTest {
     @DisplayName("모집 마감이 완료된 모임을 제외한 모임 중 마감기한이 적게 남은 순으로 목록을 조회한다")
     @Test
     void findGroupThatExcludeFinishedRecruitmentOrderByDeadline() {
-        GroupFindRequest request = new GroupFindRequest();
-        request.setExcludeFinished(true);
-        request.setOrderByDeadline(true);
+        GroupFindRequest request = new FindRequestBuilder()
+                .excludeFinished(true)
+                .orderByDeadline(true)
+                .build();
+
         List<Group> actual = groupFindService.findGroups(request).getContent();
 
         assertThat(actual).usingRecursiveComparison()
@@ -195,11 +217,12 @@ class GroupFindServiceTest {
     @DisplayName("키워드가 포함되고 모집 마감이 완료된 모임을 제외한 모임 중 마감기한이 적게 남은 순으로 목록을 조회한다")
     @Test
     void findGroupThatContainKeywordsAndExcludeFinishedRecruitmentOrderByDeadline() {
-        String keyword = "모모";
-        GroupFindRequest request = new GroupFindRequest();
-        request.setKeyword(keyword);
-        request.setExcludeFinished(true);
-        request.setOrderByDeadline(true);
+        GroupFindRequest request = new FindRequestBuilder()
+                .keyword("모모")
+                .excludeFinished(true)
+                .orderByDeadline(true)
+                .build();
+
         List<Group> actual = groupFindService.findGroups(request).getContent();
 
         assertThat(actual).usingRecursiveComparison()
@@ -209,12 +232,13 @@ class GroupFindServiceTest {
     @DisplayName("키워드가 포함되고 모집 마감이 완료된 모임을 제외한 모임 중 마감기한이 적게 남은 순으로 참여한 모임 목록을 조회한다")
     @Test
     void findParticipatedGroupsAndContainKeywordsAndExcludeFinishedRecruitmentOrderByDeadline() {
-        String keyword = "두두";
-        GroupFindRequest request = new GroupFindRequest();
-        request.setKeyword(keyword);
-        request.setExcludeFinished(true);
-        request.setOrderByDeadline(true);
-        List<Group> actual = groupFindService.findParticipatedGroups(request, momo).getContent();
+        GroupFindRequest request = new FindRequestBuilder()
+                .keyword("두두")
+                .excludeFinished(true)
+                .orderByDeadline(true)
+                .build();
+
+        List<Group> actual = groupFindService.findParticipatedGroups(request, host).getContent();
 
         assertThat(actual).usingRecursiveComparison()
                 .isEqualTo(List.of(group5));
@@ -223,25 +247,64 @@ class GroupFindServiceTest {
     @DisplayName("키워드가 포함되고 모집 마감이 완료된 모임을 제외한 모임 중 마감기한이 적게 남은 순으로 주최한 모임을 조회한다")
     @Test
     void findHostedGroupsAndContainKeywordsAndExcludeFinishedRecruitmentOrderByDeadline() {
-        String keyword = "모모";
-        GroupFindRequest request = new GroupFindRequest();
-        request.setKeyword(keyword);
-        request.setExcludeFinished(true);
-        request.setOrderByDeadline(true);
-        List<Group> actual = groupFindService.findHostedGroups(request, momo).getContent();
+        GroupFindRequest request = new FindRequestBuilder()
+                .keyword("모모")
+                .excludeFinished(true)
+                .orderByDeadline(true)
+                .build();
+
+        List<Group> actual = groupFindService.findHostedGroups(request, host).getContent();
 
         assertThat(actual).usingRecursiveComparison()
                 .isEqualTo(List.of(group2, group1));
     }
 
-    private Group constructGroup(String name, Member host, Category category, int capacity, DeadlineFixture deadline) {
-        return MOMO_STUDY.builder()
-                .name(name)
-                .category(category)
-                .capacity(capacity)
-                .deadline(deadline)
-                .duration(일주일후_하루동안)
-                .schedules(일주일후_10시부터_12시까지)
-                .toGroup(host);
+    private static class FindRequestBuilder {
+
+        private int page;
+        private Long category;
+        private String keyword;
+        private boolean excludeFinished;
+        private boolean orderByDeadline;
+
+        public FindRequestBuilder() {
+            this.page = 0;
+            this.excludeFinished = false;
+            this.orderByDeadline = false;
+        }
+
+        public FindRequestBuilder category(long category) {
+            this.category = category;
+            return this;
+        }
+
+        public FindRequestBuilder category(Category category) {
+            return category(category.getId());
+        }
+
+        public FindRequestBuilder keyword(String keyword) {
+            this.keyword = keyword;
+            return this;
+        }
+
+        public FindRequestBuilder excludeFinished(boolean value) {
+            this.excludeFinished = value;
+            return this;
+        }
+
+        public FindRequestBuilder orderByDeadline(boolean value) {
+            this.orderByDeadline = value;
+            return this;
+        }
+
+        public GroupFindRequest build() {
+            GroupFindRequest request = new GroupFindRequest();
+            request.setPage(page);
+            request.setCategory(category);
+            request.setKeyword(keyword);
+            request.setExcludeFinished(excludeFinished);
+            request.setOrderByDeadline(orderByDeadline);
+            return request;
+        }
     }
 }
