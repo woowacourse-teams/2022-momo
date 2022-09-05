4af7fa7b5e7fb3a344ff803780b82c07cf0522c8
sinb57 <sinb57@naver.com>
2022-09-05 15:42:34 +0900

    test: GroupSearchServiceTest 리팩토링

--- backend/src/test/java/com/woowacourse/momo/group/service/GroupSearchServiceTest.java
+++ backend/src/test/java/com/woowacourse/momo/group/service/GroupSearchServiceTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import static com.woowacourse.momo.fixture.GroupFixture.MOMO_STUDY;
+import static com.woowacourse.momo.fixture.MemberFixture.MOMO;
 
 import java.util.Comparator;
 import java.util.List;
@@ -13,12 +14,16 @@ import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.woowacourse.momo.auth.support.SHA256Encoder;
+import lombok.RequiredArgsConstructor;
+
 import com.woowacourse.momo.category.domain.Category;
 import com.woowacourse.momo.global.exception.exception.MomoException;
 import com.woowacourse.momo.group.domain.Group;
@@ -30,55 +35,38 @@ import com.woowacourse.momo.group.service.response.GroupResponseAssembler;
 import com.woowacourse.momo.group.service.response.GroupSummaryResponse;
 import com.woowacourse.momo.member.domain.Member;
 import com.woowacourse.momo.member.domain.MemberRepository;
-import com.woowacourse.momo.member.domain.Password;
-import com.woowacourse.momo.member.domain.UserId;
 
 @Transactional
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@RequiredArgsConstructor
 @SpringBootTest
 class GroupSearchServiceTest {
 
-    private static final int PAGE_SIZE = 12;
-    private static final int TWO_PAGE_GROUPS = 8;
+    private final GroupSearchService groupService;
+    private final GroupRepository groupRepository;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    private GroupSearchService groupService;
-
-    @Autowired
-    private GroupRepository groupRepository;
-
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @Autowired
-    private ParticipantService participantService;
-
-    private Password password;
-    private Member savedHost;
-    private Member savedMember1;
-    private Member savedMember2;
+    private Member host;
 
     @BeforeEach
     void setUp() {
-        password = Password.encrypt("momo123!", new SHA256Encoder());
-        savedHost = memberRepository.save(new Member(UserId.momo("주최자"), password, "momo"));
-        savedMember1 = memberRepository.save(new Member(UserId.momo("사용자1"), password, "momo"));
-        savedMember2 = memberRepository.save(new Member(UserId.momo("사용자2"), password, "momo"));
+        host = memberRepository.save(MOMO.toMember());
     }
 
     private Group saveGroup(String name, Category category) {
         return groupRepository.save(MOMO_STUDY.builder()
                 .name(name)
                 .category(category)
-                .toGroup(savedHost));
+                .toGroup(host));
     }
 
     @DisplayName("모임을 조회한다")
     @Test
     void findById() {
-        Group savedGroup = saveGroup("모모의 스터디", Category.STUDY);
-        GroupResponse expected = GroupResponseAssembler.groupResponse(savedGroup);
+        Group group = groupRepository.save(MOMO_STUDY.toGroup(host));
 
-        GroupResponse actual = groupService.findGroup(savedGroup.getId());
+        GroupResponse actual = groupService.findGroup(group.getId());
+        GroupResponse expected = GroupResponseAssembler.groupResponse(group);
 
         assertThat(actual).usingRecursiveComparison()
                 .isEqualTo(expected);
@@ -92,46 +80,6 @@ class GroupSearchServiceTest {
                 .hasMessage("존재하지 않는 모임입니다.");
     }
 
-    @DisplayName("모임 목록중 두번째 페이지를 조회한다")
-    @Test
-    void findAllWithPage() {
-        List<GroupSummaryResponse> summaries = IntStream.range(0, TWO_PAGE_GROUPS)
-                .mapToObj(i -> saveGroup("모모의 스터디", Category.STUDY))
-                .map(GroupResponseAssembler::groupSummaryResponse)
-                .sorted(Comparator.comparing(GroupSummaryResponse::getId).reversed())
-                .collect(Collectors.toList());
-
-        for (int i = 0; i < PAGE_SIZE; i++) {
-            saveGroup("모모의 스터디", Category.STUDY);
-        }
-
-        GroupFindRequest request = new GroupFindRequest();
-        request.setPage(1);
-        GroupPageResponse actual = groupService.findGroups(request);
-
-        assertAll(
-                () -> assertThat(actual.isHasNextPage()).isFalse(),
-                () -> assertThat(actual.getGroups()).usingRecursiveFieldByFieldElementComparator()
-                        .isEqualTo(summaries)
-        );
-    }
-
-    @DisplayName("카테고리의 그룹 목록을 조회한다")
-    @Test
-    void findGroupsByCategory() {
-        for (int i = 0; i < PAGE_SIZE + TWO_PAGE_GROUPS; i++) {
-            saveGroup("모모의 스터디", Category.STUDY);
-            saveGroup("모모의 술파티", Category.DRINK);
-        }
-
-        GroupFindRequest request = new GroupFindRequest();
-        request.setCategory(Category.DRINK.getId());
-        request.setPage(1);
-        GroupPageResponse actual = groupService.findGroups(request);
-
-        assertThat(actual.getGroups()).hasSize(TWO_PAGE_GROUPS);
-    }
-
     @DisplayName("키워드를 포함하는 이름의 모임을 조회한다")
     @Test
     void findAllByKeyword() {
@@ -143,8 +91,60 @@ class GroupSearchServiceTest {
         GroupFindRequest request = new GroupFindRequest();
         request.setKeyword("모모");
         request.setPage(0);
+
         GroupPageResponse actual = groupService.findGroups(request);
 
         assertThat(actual.getGroups()).hasSize(2);
+    }
+
+    @DisplayName("페이지 테스트")
+    @TestInstance(value = Lifecycle.PER_CLASS)
+    @Nested
+    class PageTest {
+
+        private static final int PAGE_SIZE = 12;
+        private static final int TWO_PAGE_GROUPS = 8;
+
+        @DisplayName("모임 목록중 두번째 페이지를 조회한다")
+        @Test
+        void findAllWithPage() {
+            List<GroupSummaryResponse> summaries = IntStream.range(0, TWO_PAGE_GROUPS)
+                    .mapToObj(i -> saveGroup("모모의 스터디", Category.STUDY))
+                    .map(GroupResponseAssembler::groupSummaryResponse)
+                    .sorted(Comparator.comparing(GroupSummaryResponse::getId).reversed())
+                    .collect(Collectors.toList());
+
+            for (int i = 0; i < PAGE_SIZE; i++) {
+                saveGroup("모모의 스터디", Category.STUDY);
+            }
+
+            GroupFindRequest request = new GroupFindRequest();
+            request.setPage(1);
+
+            GroupPageResponse actual = groupService.findGroups(request);
+
+            assertAll(
+                    () -> assertThat(actual.isHasNextPage()).isFalse(),
+                    () -> assertThat(actual.getGroups()).usingRecursiveFieldByFieldElementComparator()
+                            .isEqualTo(summaries)
+            );
+        }
+
+        @DisplayName("카테고리의 그룹 목록을 조회한다")
+        @Test
+        void findGroupsByCategory() {
+            for (int i = 0; i < PAGE_SIZE + TWO_PAGE_GROUPS; i++) {
+                saveGroup("모모의 스터디", Category.STUDY);
+                saveGroup("모모의 술파티", Category.DRINK);
+            }
+
+            GroupFindRequest request = new GroupFindRequest();
+            request.setCategory(Category.DRINK.getId());
+            request.setPage(1);
+
+            GroupPageResponse actual = groupService.findGroups(request);
+
+            assertThat(actual.getGroups()).hasSize(TWO_PAGE_GROUPS);
+        }
     }
 }
