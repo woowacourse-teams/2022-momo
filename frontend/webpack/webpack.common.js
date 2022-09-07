3d19b4088516ed486926d818c5cdf52fa3811838
이아현 <lah1203@naver.com>
2022-09-07 15:29:20 +0900

    refactor: 라이브러리 수정
      - 스토리북 제거
      - esbuild loader 추가
    
    Co-authored-by: Usage <kyr9389@naver.com>

--- frontend/webpack/webpack.common.js
+++ frontend/webpack/webpack.common.js
@@ -9,17 +9,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
         test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
+        type: 'asset/resource',
       },
       {
         test: /\.svg$/i,
