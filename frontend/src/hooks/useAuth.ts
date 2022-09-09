59c0e192ad12cd3fc48b54278d7b145ff0a3c87c
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-09-10 00:19:32 +0900

    refactor: Auth 페이지 리팩토링

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,11 +1,7 @@
-import {
-  useSetRecoilState,
-  useRecoilValue,
-  useResetRecoilState,
-  useRecoilState,
-} from 'recoil';
+import { useResetRecoilState, useRecoilState } from 'recoil';
+
 import { accessTokenState, refreshTokenState, loginState } from 'store/states';
-import { UserProfile } from 'types/user';
+import { LoginType, UserProfile } from 'types/user';
 
 const useAuth = () => {
   const resetLoginInfo = useResetRecoilState(loginState);
@@ -14,15 +10,20 @@ const useAuth = () => {
   const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
   const [refreshToken, setRefreshToken] = useRecoilState(refreshTokenState);
 
-  const setAuth = (
-    userInfo: UserProfile,
-    accessToken: string,
-    refreshToken: string,
-  ) => {
+  const setAuth = (accessToken: string, refreshToken: string) => {
     setAccessToken(accessToken);
     setRefreshToken(refreshToken);
+  };
 
-    setLoginInfo({ isLogin: true, loginType: 'basic', user: userInfo });
+  const setLogin = (loginType: LoginType, userInfo: UserProfile) => {
+    switch (loginType) {
+      case 'basic':
+        setLoginInfo({ isLogin: true, loginType: 'basic', user: userInfo });
+        break;
+      case 'oauth':
+        setLoginInfo({ isLogin: true, loginType: 'oauth', user: userInfo });
+        break;
+    }
   };
 
   const resetAuth = () => {
@@ -42,6 +43,7 @@ const useAuth = () => {
     setRefreshToken,
     setAuth,
     resetAuth,
+    setLogin,
   };
 };
 
