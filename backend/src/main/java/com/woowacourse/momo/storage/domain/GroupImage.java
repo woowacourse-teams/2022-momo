9e6b5da9ca3289782d4f22dfee577834e5230d25
yukong <kyb062666@gmail.com>
2022-09-10 15:18:43 +0900

    feat: 그룹 이미지 도메인 생성

---
+++ backend/src/main/java/com/woowacourse/momo/storage/domain/GroupImage.java
@@ -0,0 +1,30 @@
+package com.woowacourse.momo.storage.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import com.woowacourse.momo.group.domain.group.Group;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class GroupImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Group group;
+
+    @Column
+    private String imageName;
+}
