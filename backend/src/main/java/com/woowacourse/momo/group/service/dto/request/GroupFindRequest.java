b50018cd0e7b087af8f00ae608a731f1acdd7b88
sinb57 <sinb57@naver.com>
2022-09-13 18:27:53 +0900

    refactor: GroupFindRequest 어노테이션 배치 변경

--- backend/src/main/java/com/woowacourse/momo/group/service/dto/request/GroupFindRequest.java
+++ backend/src/main/java/com/woowacourse/momo/group/service/dto/request/GroupFindRequest.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 public class GroupFindRequest {
 
     private int page = 0;
