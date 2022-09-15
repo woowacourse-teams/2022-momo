3fae0ebfe2426c0b6c9b021ebcfd0f849deddc2a
Seongwon97 <osw970325@naver.com>
2022-09-15 16:43:48 +0900

    refactor: 참여 인원수에 대한 매직넘버에 의미 부여
    Co-authored-by: sinb57 <sinb57@naver.com>

--- backend/src/main/java/com/woowacourse/momo/group/repository/ConditionFilter.java
+++ backend/src/main/java/com/woowacourse/momo/group/repository/ConditionFilter.java
@@ -5,6 +5,7 @@ import static com.woowacourse.momo.group.domain.QGroup.group;
 import java.time.LocalDateTime;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.NumberExpression;
@@ -57,8 +58,10 @@ public class ConditionFilter {
 
     private BooleanExpression isNotParticipantsFull() {
         NumberPath<Integer> capacity = group.participants.capacity.value;
-        NumberExpression<Integer> participantsSize = group.participants.participants.size()
-                .add(Expressions.constant(1));
+
+        NumberExpression<Integer> sizeWithoutHost = group.participants.participants.size();
+        Expression<Integer> hostSize = Expressions.constant(1);
+        NumberExpression<Integer> participantsSize = sizeWithoutHost.add(hostSize);
 
         return capacity.gt(participantsSize);
     }
