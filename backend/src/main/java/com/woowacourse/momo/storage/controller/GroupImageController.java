1ebbcc0fd97105cf8a1f0aaa44af284c80ee7089
yukong <kyb062666@gmail.com>
2022-09-15 11:21:37 +0900

    feat: 그룹 이미지 저장하는 API 추가
    Co-authored-by: nbalance97 <nbalance__@kyonggi.ac.kr>

--- backend/src/main/java/com/woowacourse/momo/storage/controller/GroupImageController.java
+++ backend/src/main/java/com/woowacourse/momo/storage/controller/GroupImageController.java
@@ -1,14 +1,19 @@
 package com.woowacourse.momo.storage.controller;
 
+import java.net.URI;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 
+import com.woowacourse.momo.auth.config.AuthenticationPrincipal;
 import com.woowacourse.momo.storage.service.GroupImageService;
 
 @RestController
@@ -17,6 +22,14 @@ import com.woowacourse.momo.storage.service.GroupImageService;
 public class GroupImageController {
 
     private final GroupImageService groupImageService;
+
+    @PostMapping
+    public ResponseEntity<Void> imageSave(
+            @PathVariable Long groupId, @AuthenticationPrincipal Long memberId, MultipartFile multipartFile
+    ) {
+        groupImageService.save(memberId, groupId, multipartFile);
+        return ResponseEntity.created(URI.create("/api/groups/" + groupId + "/thumnail")).build();
+    }
 
     @GetMapping(
             produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE}
