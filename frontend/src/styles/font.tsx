b763944fe43032e8d003427032ed224c142d5741
이아현 <lah1203@naver.com>
2022-09-15 10:32:15 +0900

    feat: 폰트 최적화
    Co-authored-by: Usage <kyr9389@naver.com>

--- frontend/src/styles/font.tsx
+++ frontend/src/styles/font.tsx
@@ -1,20 +1,26 @@
 import { css } from '@emotion/react';
 
+const { origin } = window.location;
+
 const fontStyle = css`
   @font-face {
     font-family: 'GangwonEdu_Bold';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/GangwonEdu_OTFBoldA.woff')
-      format('woff');
+    src: url('${origin}/font/GangwonEdu_Bold.woff2') format('woff2'),
+      url('${origin}/font/GangwonEdu_Bold.ttf') format('truetype');
     font-weight: normal;
     font-style: normal;
+    font-display: fallback;
   }
 
   @font-face {
     font-family: 'SuseongDotum';
     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2205@1.0/SuseongDotum.woff2')
-      format('woff2');
+        format('woff2'),
+      url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2205@1.0/SuseongDotum.ttf')
+        format('ttf');
     font-weight: normal;
     font-style: normal;
+    font-display: fallback;
   }
 `;
 
