2e3c3f22ef364c1a98cdc77e730e0f3bec2e6e11
이아현 <lah1203@naver.com>
2022-09-15 10:33:23 +0900

    chore: js 파일명 해시 적용
    Co-authored-by: Usage <kyr9389@naver.com>

--- frontend/webpack/webpack.common.js
+++ frontend/webpack/webpack.common.js
@@ -42,7 +42,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../dist'),
     publicPath: '/',
-    filename: 'bundle.js',
+    filename: '[contenthash].bundle.js',
   },
   plugins: [
     new ForkTsCheckerWebpackPlugin(),
