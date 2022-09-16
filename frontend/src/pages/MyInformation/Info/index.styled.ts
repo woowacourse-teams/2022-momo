ee02daa7781f405a21de12c73823be14b4e05248
이아현 <lah1203@naver.com>
2022-09-16 16:25:06 +0900

    feat: 마이페이지 리렌더링 최적화
    Co-authored-by: Usage <kyr9389@naver.com>

--- frontend/src/pages/MyInformation/Info/index.styled.ts
+++ frontend/src/pages/MyInformation/Info/index.styled.ts
@@ -1,59 +1,12 @@
-import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
-
-const kongKong = keyframes`
-  0% {
-    transform: translate3d(0, 0, 0);
-  }
-
-  20% {
-    transform: translate3d(0.2rem, -1rem, 0);
-  }
-
-  30% {
-    transform: translate3d(0.3rem, -1rem, 0);
-  }
-
-  50% {
-    transform: translate3d(0.5rem, 0, 0);
-  }
-
-  70% {
-    transform: translate3d(0.7rem, -1rem, 0);
-  }
-
-  80% {
-    transform: translate3d(0.8rem, -1rem, 0);
-  }
-
-  100% {
-    transform: translate3d(1rem, 0, 0);
-  }
-`;
 
 const Container = styled.div`
   display: flex;
   gap: 3rem;
 `;
 
-const LiveBeanBox = styled.div`
-  display: flex;
-`;
-
-const LiveBean = styled.div`
-  position: relative;
-
-  bottom: 0;
-
-  animation: 1.5s ${kongKong} ease-out infinite alternate;
-
-  &.reverse {
-    animation: 1.5s ${kongKong} ease-out infinite alternate-reverse;
-  }
-`;
-
 const Right = styled.div`
   display: flex;
 `;
 
-export { Container, LiveBeanBox, LiveBean, Right };
+export { Container, Right };
