0c3240fb7d7dbb9d7a888c49907af3ccfd918ff7
이아현 <lah1203@naver.com>
2022-09-19 13:44:21 +0900

    fix: MSW 비활성화
    Co-authored-by: Usage <kyr9389@naver.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,6 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('mocks/browser');
-  worker.start({
-    onUnhandledRequest: 'bypass',
-  });
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(<App />);
