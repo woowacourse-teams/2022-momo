86b3fa6cc65891481a181c0e31215e2c90679d3d
이아현 <lah1203@naver.com>
2022-09-22 14:04:54 +0900

    chore: 카카오맵 타입 추가

---
+++ frontend/src/types/kakaomap.ts
@@ -0,0 +1,31 @@
+import { Address } from 'react-daum-postcode';
+
+export type RoadAaddress = {
+  address_name: string;
+  region_1depth_name: string;
+  region_2depth_name: string;
+  region_3depth_name: string;
+  road_name: string;
+  underground_yn: 'Y' | 'N';
+  main_building_no: string;
+  sub_building_no: string;
+  building_name: string;
+  zone_no: string;
+  x: string;
+  y: string;
+};
+
+export type GeocoderResult = Array<{
+  address_name: string;
+  address_type: 'REGION' | 'ROAD' | 'REGION_ADDR' | 'ROAD_ADDR';
+  x: string;
+  y: string;
+  address: Address;
+  road_address: RoadAaddress;
+}>;
+
+export enum GeocoderStatus {
+  ERROR = 'ERROR',
+  OK = 'OK',
+  ZERO_RESULT = 'ZERO_RESULT',
+}
