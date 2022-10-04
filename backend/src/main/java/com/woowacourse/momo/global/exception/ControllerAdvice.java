60735e2eedb6f659a076292248fedf56d6713211
sinb57 <sinb57@naver.com>
2022-10-04 19:32:07 +0900

    feat: 지원하지 않는 API 요청 처리

--- backend/src/main/java/com/woowacourse/momo/global/exception/ControllerAdvice.java
+++ backend/src/main/java/com/woowacourse/momo/global/exception/ControllerAdvice.java
@@ -4,8 +4,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.woowacourse.momo.global.exception.dto.response.ExceptionResponse;
+import com.woowacourse.momo.global.exception.exception.ErrorCode;
 import com.woowacourse.momo.global.exception.exception.GlobalErrorCode;
 import com.woowacourse.momo.global.exception.exception.MomoException;
 import com.woowacourse.momo.global.logging.UnhandledErrorLogging;
@@ -20,12 +22,20 @@ public class ControllerAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        return ResponseEntity.badRequest().body(ExceptionResponse.from(GlobalErrorCode.VALIDATION_ERROR.getErrorCode()));
+        return ResponseEntity.badRequest()
+                .body(ExceptionResponse.from(GlobalErrorCode.VALIDATION_ERROR.getErrorCode()));
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ExceptionResponse> unhandledApiException(NoHandlerFoundException exception) {
+        ErrorCode e = GlobalErrorCode.UNHANDLED_API_ERROR;
+        return ResponseEntity.status(e.getStatusCode()).body(ExceptionResponse.from(e.getErrorCode()));
     }
 
     @UnhandledErrorLogging
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ExceptionResponse> handleAnyException(Exception e) {
-        return ResponseEntity.internalServerError().body(ExceptionResponse.from(GlobalErrorCode.INTERNAL_SERVER_ERROR.getErrorCode()));
+        return ResponseEntity.internalServerError()
+                .body(ExceptionResponse.from(GlobalErrorCode.INTERNAL_SERVER_ERROR.getErrorCode()));
     }
 }
--- backend/src/main/java/com/woowacourse/momo/global/exception/exception/GlobalErrorCode.java
+++ backend/src/main/java/com/woowacourse/momo/global/exception/exception/GlobalErrorCode.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum GlobalErrorCode implements ErrorCode {
 
+    UNHANDLED_API_ERROR(404, "NOT_FOUND", "지원하지 않는 API 요청입니다."),
     VALIDATION_ERROR(400, "VALIDATION_001", "잘못된 요청입니다."),
     INTERNAL_SERVER_ERROR(500, "SERVER_001", "내부 서버 오류입니다."),
     ;
