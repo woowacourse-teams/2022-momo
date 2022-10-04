63d6995720f9a85ea75d8103a5add3ee6ba5eb01
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-10-04 19:37:29 +0900

    커밋

--- backend/src/main/java/com/woowacourse/momo/MomoApplication.java
+++ backend/src/main/java/com/woowacourse/momo/MomoApplication.java
@@ -1,10 +1,20 @@
 package com.woowacourse.momo;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MomoApplication {
+
+    @PostConstruct
+    public void started() {
+        // timezone UTC 셋팅
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MomoApplication.class, args);
