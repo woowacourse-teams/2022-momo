9f5faa870abbc3642f3700297148cf13d2f78d6f
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-10-04 20:07:13 +0900

    refactor: TimeZone 설정 파트 추가

--- backend/src/main/java/com/woowacourse/momo/MomoApplication.java
+++ backend/src/main/java/com/woowacourse/momo/MomoApplication.java
@@ -1,10 +1,19 @@
 package com.woowacourse.momo;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MomoApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MomoApplication.class, args);
