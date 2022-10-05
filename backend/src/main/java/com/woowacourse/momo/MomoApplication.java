824c4950dec2921dd2691bd49b3f735a58618ff8
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-10-05 10:08:52 +0900

    refactor: Config 분리

--- backend/src/main/java/com/woowacourse/momo/MomoApplication.java
+++ backend/src/main/java/com/woowacourse/momo/MomoApplication.java
@@ -1,19 +1,10 @@
 package com.woowacourse.momo;
 
-import java.util.TimeZone;
-
-import javax.annotation.PostConstruct;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MomoApplication {
-
-    @PostConstruct
-    public void started() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(MomoApplication.class, args);
