3ea680e784f800f5e59c3a2305ef9d968a9c2915
Seongwon97 <osw970325@naver.com>
2022-10-06 16:41:44 +0900

    refactor: FavoriteRepository의 상위 클래스를 JpaRepository에서 Repository로 변경

--- backend/src/main/java/com/woowacourse/momo/favorite/domain/FavoriteRepository.java
+++ backend/src/main/java/com/woowacourse/momo/favorite/domain/FavoriteRepository.java
@@ -3,15 +3,19 @@ package com.woowacourse.momo.favorite.domain;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
 
-public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+public interface FavoriteRepository extends Repository<Favorite, Long> {
+
+    Favorite save(Favorite favorite);
 
     boolean existsByGroupIdAndMemberId(Long groupId, Long memberId);
 
     Optional<Favorite> findByGroupIdAndMemberId(Long groupId, Long memberId);
 
     List<Favorite> findAllByMemberId(Long memberId);
+
+    void delete(Favorite favorite);
 
     void deleteAllByGroupId(Long groupId);
 
