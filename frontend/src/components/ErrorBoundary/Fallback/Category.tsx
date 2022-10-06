51dd910b64acbd7ee8c14a985f41b61f224758a2
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-10-06 20:01:14 +0900

    feat: 카테고리 에러 바운더리 수정

--- frontend/src/components/ErrorBoundary/Fallback/Category.tsx
+++ frontend/src/components/ErrorBoundary/Fallback/Category.tsx
@@ -1,7 +1,14 @@
 import * as S from './index.styled';
 
 function CategoryFallback() {
-  return <S.CategoryContainer>에러가 발생했어요 😥</S.CategoryContainer>;
+  return (
+    <S.CategoryContainer>
+      <p>카테고리 목록을 불러오는 중 에러가 발생했어요 😥</p>
+      <S.RefreshButton onClick={() => window.location.reload()}>
+        새로고침
+      </S.RefreshButton>
+    </S.CategoryContainer>
+  );
 }
 
 export { CategoryFallback };
