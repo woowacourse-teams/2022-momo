57dc7bb6108773ab2c719e808b86591f69388b0b
Seongwon97 <osw970325@naver.com>
2022-10-06 15:56:41 +0900

    feat: id를 통한 회원 존재 유무 로직 수정

--- backend/src/main/java/com/woowacourse/momo/member/service/MemberValidator.java
+++ backend/src/main/java/com/woowacourse/momo/member/service/MemberValidator.java
@@ -1,10 +1,13 @@
 package com.woowacourse.momo.member.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
+import com.woowacourse.momo.member.domain.Member;
 import com.woowacourse.momo.member.domain.MemberRepository;
 import com.woowacourse.momo.member.exception.MemberErrorCode;
 import com.woowacourse.momo.member.exception.MemberException;
@@ -17,8 +20,8 @@ public class MemberValidator {
     private final MemberRepository memberRepository;
 
     public void validateExistMember(Long memberId) {
-        boolean result = memberRepository.existsById(memberId);
-        if (!result) {
+        Optional<Member> member = memberRepository.findById(memberId);
+        if (member.isEmpty() || member.get().isDeleted()) {
             throw new MemberException(MemberErrorCode.MEMBER_NOT_EXIST);
         }
     }
