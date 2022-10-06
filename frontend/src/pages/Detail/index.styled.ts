e6a27e8690b4fa0dd5ac47d44bfa36a26b5b161e
이아현 <lah1203@naver.com>
2022-10-06 20:07:20 +0900

    style: 모임 상세 페이지 재구성

--- frontend/src/pages/Detail/index.styled.ts
+++ frontend/src/pages/Detail/index.styled.ts
@@ -1,11 +1,81 @@
 import styled from '@emotion/styled';
 
-const PageContainer = styled.div`
-  display: grid;
-  grid-template-columns: 2fr 5fr;
-  gap: 2rem;
+const Image = styled.div<{ src: string }>`
+  position: sticky;
+  top: 4rem;
 
-  padding: 2rem 10%;
+  width: 100%;
+  height: 5rem;
+
+  background: ${({ theme: { filter }, src }) =>
+    `linear-gradient(${filter.darken001}, ${filter.darken001}), url(${src})`};
+  background-size: cover;
+  background-position: center;
 `;
 
-export { PageContainer };
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+
+  padding: 1rem;
+`;
+
+const ContentContainer = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  gap: 2rem;
+
+  width: 1000px;
+`;
+
+const StickyContainer = styled(ContentContainer)`
+  align-items: center;
+
+  position: sticky;
+  top: 11rem;
+
+  background: ${({ theme: { colors } }) => colors.white001};
+
+  z-index: 9;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  margin: 1rem 0;
+`;
+
+const Category = styled.div`
+  color: ${({ theme: { colors } }) => colors.blue001};
+
+  font-weight: 900;
+`;
+
+const Title = styled.h2`
+  line-height: 2rem;
+
+  font-size: 1.7rem;
+`;
+
+const Duration = styled.div`
+  align-items: center;
+
+  color: ${({ theme: { colors } }) => colors.red003};
+
+  font-weight: 700;
+`;
+
+export {
+  Image,
+  Container,
+  ContentContainer,
+  StickyContainer,
+  Header,
+  Category,
+  Title,
+  Duration,
+};
--- frontend/src/pages/Detail/index.tsx
+++ frontend/src/pages/Detail/index.tsx
@@ -1,16 +1,23 @@
 import { useEffect } from 'react';
 
-import { useQueryClient } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 import { useParams } from 'react-router-dom';
 
-import { requestGroupDetail } from 'apis/request/group';
+import {
+  requestGroupDetail,
+  requestGroupParticipants,
+} from 'apis/request/group';
 import { QUERY_KEY } from 'constants/key';
 import useCategory from 'hooks/useCategory';
 import useRecoilQuery from 'hooks/useRecoilQuery';
 import { groupDetailState } from 'store/states';
+import { GroupParticipants } from 'types/data';
+import { getCategoryImage } from 'utils/category';
+import { convertDeadlineToRemainTime } from 'utils/date';
 import { accessTokenProvider } from 'utils/token';
 
 import Content from './Content';
+import ControlButton from './ControlButton';
 import * as S from './index.styled';
 import LikeButton from './LikeButton';
 import SideBar from './SideBar';
@@ -18,49 +25,73 @@ import SideBar from './SideBar';
 function Detail() {
   const { id } = useParams();
 
-  const { state: data, refetch } = useRecoilQuery(
+  const { state: data } = useRecoilQuery(
     groupDetailState,
     QUERY_KEY.GROUP_DETAILS,
     () => requestGroupDetail(Number(id)),
     0,
   );
+  const { data: participants } = useQuery<GroupParticipants>(
+    `${QUERY_KEY.GROUP_PARTICIPANTS}/${id}`,
+    () => requestGroupParticipants(Number(id)),
+    { staleTime: Infinity },
+  );
+
   const { categories } = useCategory();
+
   const queryClient = useQueryClient();
 
   useEffect(() => {
-    queryClient.invalidateQueries([QUERY_KEY.GROUP_DETAILS]);
+    queryClient.invalidateQueries(QUERY_KEY.GROUP_DETAILS);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [accessTokenProvider.get()]);
 
   return (
-    <S.PageContainer>
-      {data && (
-        <>
-          <SideBar
-            id={Number(id)}
-            host={data.host}
-            capacity={data.capacity}
-            duration={data.duration}
-            schedules={data.schedules}
-            finished={data.finished}
-            location={data.location}
-            categoryName={
-              categories.find(category => category.id === data.categoryId)
-                ?.name || ''
-            }
-          />
-          <Content
-            name={data.name}
-            deadline={data.deadline}
-            finished={data.finished}
-            categoryId={data.categoryId}
-            location={data.location}
-            description={data.description}
-          />
-          <LikeButton id={Number(id)} like={data.like} refetch={refetch} />
-        </>
-      )}
-    </S.PageContainer>
+    <>
+      <S.Image src={getCategoryImage(data.categoryId)} />
+      <S.Container>
+        {data && participants && (
+          <>
+            <S.StickyContainer>
+              <S.Header>
+                <S.Category>
+                  {categories.find(category => category.id === data.categoryId)
+                    ?.name || ''}
+                </S.Category>
+                <S.Title>{data.name}</S.Title>
+                <S.Duration>
+                  모집{' '}
+                  {data.finished
+                    ? '마감 완료'
+                    : convertDeadlineToRemainTime(data.deadline)}
+                </S.Duration>
+              </S.Header>
+              <ControlButton
+                id={Number(id)}
+                host={data.host}
+                capacity={data.capacity}
+                finished={data.finished}
+                participants={participants}
+              />
+            </S.StickyContainer>
+            <S.ContentContainer>
+              <Content
+                location={data.location}
+                description={data.description}
+              />
+              <SideBar
+                host={data.host}
+                capacity={data.capacity}
+                duration={data.duration}
+                schedules={data.schedules}
+                participants={participants}
+              />
+              <LikeButton id={Number(id)} like={data.like} />
+            </S.ContentContainer>
+          </>
+        )}
+      </S.Container>
+    </>
   );
 }
 
