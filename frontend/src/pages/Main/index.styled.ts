771c4f2572e0e36b820a28ec7504c34cd73607b1
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-10-06 19:45:04 +0900

    feat: 메인 페이지 디자인 변경

--- frontend/src/pages/Main/index.styled.ts
+++ frontend/src/pages/Main/index.styled.ts
@@ -4,25 +4,7 @@ const Content = styled.div`
   width: 100%;
 
   margin: 2rem auto;
-
-  ${({ theme: { breakpoints } }) => `
-    @media only screen and (max-width: ${breakpoints.md}px) {
-      align-items: center;
-      max-width: ${breakpoints.sm}px;
-    }
-
-    @media only screen and (min-width: ${breakpoints.md}px) and (max-width: ${breakpoints.lg}px) {
-      max-width: ${breakpoints.md}px;
-    }
-
-    @media only screen and (min-width: ${breakpoints.lg}px) and (max-width: ${breakpoints.xl}px) {
-      max-width: ${breakpoints.lg}px;
-    }
-
-    @media only screen and (min-width: ${breakpoints.xl}px) {
-      max-width: ${breakpoints.xl}px;
-    }
-  `}
+  padding-top: 10rem;
 `;
 
 export { Content };
