6de100306338591a54385b667a0794a702c71c34
이아현 <lah1203@naver.com>
2022-10-11 15:01:28 +0900

    feat: 태블릿 이상 크기는 데스크톱, 이하 크기는 모바일 버전으로 보여주도록 구현

--- frontend/src/pages/Detail/index.tsx
+++ frontend/src/pages/Detail/index.tsx
@@ -8,19 +8,13 @@ import {
   requestGroupParticipants,
 } from 'apis/request/group';
 import { QUERY_KEY } from 'constants/key';
-import useCategory from 'hooks/useCategory';
 import useRecoilQuery from 'hooks/useRecoilQuery';
 import { groupDetailState } from 'store/states';
 import { GroupParticipants } from 'types/data';
-import { getCategoryImage } from 'utils/category';
-import { convertDeadlineToRemainTime } from 'utils/date';
 import { accessTokenProvider } from 'utils/token';
 
-import Content from './Content';
-import ControlButton from './ControlButton';
-import * as S from './index.styled';
-import LikeButton from './LikeButton';
-import SideBar from './SideBar';
+import Desktop from './Versions/Desktop';
+import Mobile from './Versions/Mobile';
 
 function Detail() {
   const { id } = useParams();
@@ -37,8 +31,6 @@ function Detail() {
     { staleTime: Infinity },
   );
 
-  const { categories } = useCategory();
-
   const queryClient = useQueryClient();
 
   useEffect(() => {
@@ -46,53 +38,13 @@ function Detail() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [accessTokenProvider.get()]);
 
-  return (
-    <>
-      <S.Image src={getCategoryImage(data.categoryId)} />
-      <S.Container>
-        {data && participants && (
-          <>
-            <S.StickyContainer>
-              <S.Header>
-                <S.Category>
-                  {categories.find(category => category.id === data.categoryId)
-                    ?.name || ''}
-                </S.Category>
-                <S.Title>{data.name}</S.Title>
-                <S.Duration>
-                  모집{' '}
-                  {data.finished
-                    ? '마감 완료'
-                    : convertDeadlineToRemainTime(data.deadline)}
-                </S.Duration>
-              </S.Header>
-              <ControlButton
-                id={Number(id)}
-                host={data.host}
-                capacity={data.capacity}
-                finished={data.finished}
-                participants={participants}
-              />
-            </S.StickyContainer>
-            <S.ContentContainer>
-              <Content
-                location={data.location}
-                description={data.description}
-              />
-              <SideBar
-                host={data.host}
-                capacity={data.capacity}
-                duration={data.duration}
-                schedules={data.schedules}
-                participants={participants}
-              />
-              <LikeButton id={Number(id)} like={data.like} />
-            </S.ContentContainer>
-          </>
-        )}
-      </S.Container>
-    </>
-  );
+  if (!participants) return <></>;
+
+  if (document.body.clientWidth > 768) {
+    return <Desktop id={Number(id)} data={data} participants={participants} />;
+  }
+
+  return <Mobile id={Number(id)} data={data} participants={participants} />;
 }
 
 export default Detail;
