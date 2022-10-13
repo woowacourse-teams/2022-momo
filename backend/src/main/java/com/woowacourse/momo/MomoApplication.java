927e2ae885caf2cfa2db1bc32e3304c615b7a538
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-10-13 18:09:59 +0900

    feat: postconstruct로 timezone 설정

--- backend/src/main/java/com/woowacourse/momo/MomoApplication.java
+++ backend/src/main/java/com/woowacourse/momo/MomoApplication.java
@@ -1,10 +1,19 @@
 package com.woowacourse.momo;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MomoApplication {
+
+    @PostConstruct
+    public void setUp() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MomoApplication.class, args);
