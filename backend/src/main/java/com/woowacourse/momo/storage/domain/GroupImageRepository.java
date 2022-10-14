3c6ae9a2c5accf1f65511fa9b7710e222c7026b9
yukong <kyb062666@gmail.com>
2022-10-14 15:49:36 +0900

    feat: 그룹 이미지 객체를 생성하지 않고 조회하는 메서드 추가

--- backend/src/main/java/com/woowacourse/momo/storage/domain/GroupImageRepository.java
+++ backend/src/main/java/com/woowacourse/momo/storage/domain/GroupImageRepository.java
@@ -3,10 +3,15 @@ package com.woowacourse.momo.storage.domain;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface GroupImageRepository extends JpaRepository<GroupImage, Long> {
 
     Optional<GroupImage> findByGroupId(Long groupId);
+
+    @Query("select gi.imageName from GroupImage gi where gi.groupId = :groupId")
+    Optional<String> findImageNameByGroupId(@Param("groupId") Long groupId);
 
     void deleteByGroupId(Long groupId);
 }
