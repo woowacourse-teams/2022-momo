9cccbb809fd5fd81c0ef78b8ee41fc8ecbda98aa
yukong <kyb062666@gmail.com>
2022-10-15 19:21:47 +0900

    refactor: Category indent 맞추기

--- backend/src/main/java/com/woowacourse/momo/category/domain/Category.java
+++ backend/src/main/java/com/woowacourse/momo/category/domain/Category.java
@@ -12,16 +12,16 @@ import com.woowacourse.momo.category.exception.CategoryException;
 @RequiredArgsConstructor
 public enum Category {
 
-     STUDY(1, "스터디", "study.jpg", "study.png"),
-     MOCO(2, "모각코", "moco.jpg", "moco.png"),
-     EAT(3, "식사", "eat.jpg", "eat.png"),
-     CAFE(4, "카페", "cafe.jpg", "cafe.png"),
-     DRINK(5, "술", "drink.jpg", "drink.png"),
-     HEALTH(6, "운동", "health.jpg", "health.png"),
-     GAME(7, "게임", "game.jpg", "game.png"),
-     TRAVEL(8, "여행", "travel.jpg", "travel.png"),
-     CULTURE(9, "문화생활", "culture.jpg", "culture.png"),
-     ETC(10, "기타", "etc.jpg", "etc.png");
+    STUDY(1, "스터디", "study.jpg", "study.png"),
+    MOCO(2, "모각코", "moco.jpg", "moco.png"),
+    EAT(3, "식사", "eat.jpg", "eat.png"),
+    CAFE(4, "카페", "cafe.jpg", "cafe.png"),
+    DRINK(5, "술", "drink.jpg", "drink.png"),
+    HEALTH(6, "운동", "health.jpg", "health.png"),
+    GAME(7, "게임", "game.jpg", "game.png"),
+    TRAVEL(8, "여행", "travel.jpg", "travel.png"),
+    CULTURE(9, "문화생활", "culture.jpg", "culture.png"),
+    ETC(10, "기타", "etc.jpg", "etc.png");
 
     private final long id;
     private final String name;
