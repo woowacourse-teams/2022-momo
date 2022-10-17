d0446cb034f2749265fccb2b23c17e1d8d9f55fb
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-10-17 22:03:49 +0900

    feat: 생성 컴포넌트가 스크롤 대신 페이지에 따른 Step 컴포넌트를 출력하도록 변경

---
+++ frontend/src/pages/Create/Steps/CreateSteps.tsx
@@ -0,0 +1,78 @@
+import { CreateStateReturnValues } from 'hooks/useCreateState';
+import { GroupDetailData } from 'types/data';
+
+import { Step1, Step2, Step3, Step4, Step5 } from './';
+
+interface CreateStepsProp {
+  useNameState: CreateStateReturnValues['useNameState'];
+  useSelectedCategoryState: CreateStateReturnValues['useSelectedCategoryState'];
+  useCapacityState: CreateStateReturnValues['useCapacityState'];
+  useDateState: CreateStateReturnValues['useDateState'];
+  useScheduleState: CreateStateReturnValues['useScheduleState'];
+  useDeadlineState: CreateStateReturnValues['useDeadlineState'];
+  useLocationState: CreateStateReturnValues['useLocationState'];
+  useDescriptionState: CreateStateReturnValues['useDescriptionState'];
+  pressEnterToNext: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  gotoNextPage: () => void;
+  duration: GroupDetailData['duration'];
+  page: number;
+}
+
+function CreateSteps({
+  useNameState,
+  useSelectedCategoryState,
+  useCapacityState,
+  useDateState,
+  useScheduleState,
+  useDeadlineState,
+  useLocationState,
+  useDescriptionState,
+  pressEnterToNext,
+  gotoNextPage,
+  duration,
+  page,
+}: CreateStepsProp) {
+  switch (page) {
+    case 1:
+      return (
+        <Step1
+          useNameState={useNameState}
+          pressEnterToNext={pressEnterToNext}
+          useSelectedCategoryState={useSelectedCategoryState}
+          gotoNextPage={gotoNextPage}
+        />
+      );
+    case 2:
+      return (
+        <Step2
+          useDateState={useDateState}
+          useDeadlineState={useDeadlineState}
+          pressEnterToNext={pressEnterToNext}
+        />
+      );
+    case 3:
+      return (
+        <Step3
+          useScheduleState={useScheduleState}
+          duration={duration}
+          pressEnterToNext={pressEnterToNext}
+        />
+      );
+    case 4:
+      return (
+        <Step4
+          useCapacityState={useCapacityState}
+          useLocationState={useLocationState}
+          pressEnterToNext={pressEnterToNext}
+        />
+      );
+    case 5: {
+      return <Step5 useDescriptionState={useDescriptionState} />;
+    }
+    default: {
+      return <></>;
+    }
+  }
+}
+
+export default CreateSteps;
--- frontend/src/pages/Create/index.styled.ts
+++ frontend/src/pages/Create/index.styled.ts
@@ -4,27 +4,100 @@ const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
   gap: 2rem;
 
-  height: 100%;
+  padding-bottom: 60px;
 `;
 
-const ScrollContainer = styled.div`
+const ButtonContainer = styled.div`
   display: flex;
-  align-items: center;
-  overflow: hidden;
-  scroll-behavior: smooth;
+  flex-direction: column;
+  gap: 0.25rem;
 
-  width: 100%;
-  min-height: fit-content;
-
-  div {
-    width: 100%;
-  }
-
-  ::-webkit-scrollbar {
-    display: none;
-  }
+  padding: 2rem 0;
 `;
 
-export { PageContainer, ScrollContainer };
+const NextPageButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.25rem;
+
+  ${({
+    theme: {
+      colors,
+      breakpoints: { md },
+    },
+  }) => `
+    &.invalid {
+      color: ${colors.gray004};
+
+      svg > g > path {
+        fill: ${colors.gray004};
+      }
+    }
+
+    &.last-page {
+      display: none;
+    }
+    
+    @media only screen and (min-width: ${md}px) {
+      h3 {
+        font-size: 1.2rem;
+      }
+
+      svg {
+        width: 30px;
+        height: 30px;
+      }
+    }
+  `}
+`;
+
+const SubmitButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.25rem;
+
+  ${({
+    theme: {
+      colors,
+      breakpoints: { md },
+    },
+  }) => `
+    span {
+      color: ${colors.green001};
+    }
+
+    svg > circle {
+      fill: ${colors.green001};
+    }
+
+    &.invalid {
+      color: ${colors.gray004};
+
+      span {
+        color: ${colors.gray005};
+      }
+
+      svg > circle {
+        fill: ${colors.gray004};
+      }
+    }
+
+    @media only screen and (min-width: ${md}px) {
+      h3 {
+        font-size: 1.2rem;
+      }
+
+      svg {
+        width: 30px;
+        height: 30px;
+      }
+    }
+  `}
+`;
+
+export { PageContainer, ButtonContainer, NextPageButton, SubmitButton };
