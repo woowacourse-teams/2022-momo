187c7b7d4c331a53901c2af7d99517d30f0c713e
yukong <kyb062666@gmail.com>
2022-10-18 14:27:07 +0900

    refactor: GroupImageEventListener 병합

--- backend/src/main/java/com/woowacourse/momo/storage/event/GroupImageEventListener.java
+++ backend/src/main/java/com/woowacourse/momo/storage/event/GroupImageEventListener.java
@@ -5,14 +5,23 @@ import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 
+import com.woowacourse.momo.group.event.GroupCreateEvent;
 import com.woowacourse.momo.group.event.GroupDeleteEvent;
+import com.woowacourse.momo.storage.domain.GroupImage;
 import com.woowacourse.momo.storage.domain.GroupImageRepository;
 
 @RequiredArgsConstructor
 @Component
-public class GroupImageDeleteEventListener {
+public class GroupImageEventListener {
 
     private final GroupImageRepository groupImageRepository;
+
+    @EventListener
+    public void createGroupImage(GroupCreateEvent event) {
+        GroupImage groupImage = new GroupImage(event.getGroupId(), event.getCategory().getDefaultImageName());
+
+        groupImageRepository.save(groupImage);
+    }
 
     @EventListener
     public void deleteGroupImage(GroupDeleteEvent event) {
