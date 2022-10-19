3544ac68fde9b423b4c8075c409da12fdce73037
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-10-19 18:29:27 +0900

    feat: IntersectionObserver 방식으로 전환

--- frontend/src/pages/Landing/index.tsx
+++ frontend/src/pages/Landing/index.tsx
@@ -1,58 +1,63 @@
-import { useState } from 'react';
+import { RefObject, useEffect, useMemo, useRef, useState } from 'react';
 
-import useMount from 'hooks/useMount';
-import useThrottle from 'hooks/useThrottle';
-
+import { Divider } from './@shared/index.styled';
 import { Step1, Step2, Step3, Step4, Step5 } from './Steps';
-
-const scrollHeightByPage = {
-  1: 300,
-  2: 1000,
-  3: 1800,
-  4: 2500,
-};
 
 function Landing() {
   const [showedPageNumbers, setShowedPageNumbers] = useState<number[]>([1]);
 
-  const throttledScrollEvent = useThrottle(() => {
-    const scrollHeight = window.scrollY;
+  const target1 = useRef<HTMLDivElement>(null);
+  const target2 = useRef<HTMLDivElement>(null);
+  const target3 = useRef<HTMLDivElement>(null);
+  const target4 = useRef<HTMLDivElement>(null);
+  const target5 = useRef<HTMLDivElement>(null);
 
-    if (scrollHeight < scrollHeightByPage[1]) {
-      setShowedPageNumbers([1]);
-      return;
+  const targetArray: Array<RefObject<HTMLDivElement>> = useMemo(
+    () => [target1, target2, target3, target4, target5],
+    [],
+  );
+  console.log(showedPageNumbers);
+  useEffect(() => {
+    const observers: IntersectionObserver[] = [];
+
+    const onIntersection = (targetIndex: number) => () => {
+      const currentDisplayedSteps = [...Array(targetIndex + 1)].map(
+        (_, i) => i + 1,
+      );
+
+      setShowedPageNumbers(currentDisplayedSteps);
+    };
+
+    if (targetArray.length > 0) {
+      targetArray.forEach((item, i) => {
+        observers[i] = new IntersectionObserver(onIntersection(i), {
+          threshold: 0.5,
+        });
+
+        if (!item.current) {
+          return;
+        }
+
+        observers[i].observe(item.current);
+      });
     }
 
-    if (scrollHeight < scrollHeightByPage[2]) {
-      setShowedPageNumbers([1, 2]);
-      return;
-    }
-
-    if (scrollHeight < scrollHeightByPage[3]) {
-      setShowedPageNumbers([1, 2, 3]);
-      return;
-    }
-
-    if (scrollHeight < scrollHeightByPage[4]) {
-      setShowedPageNumbers([1, 2, 3, 4]);
-      return;
-    }
-
-    setShowedPageNumbers([1, 2, 3, 4, 5]);
-  }, 100);
-
-  useMount(() => {
-    window.addEventListener('scroll', throttledScrollEvent);
-
-    return () => window.removeEventListener('scroll', throttledScrollEvent);
-  });
+    return () => {
+      observers && observers.forEach(item => item.disconnect);
+    };
+  }, [targetArray]);
 
   return (
     <>
+      <Divider ref={targetArray[0]} />
       <Step1 show={showedPageNumbers.includes(1)} />
+      <Divider ref={targetArray[1]} />
       <Step2 show={showedPageNumbers.includes(2)} />
+      <Divider ref={targetArray[2]} />
       <Step3 show={showedPageNumbers.includes(3)} />
+      <Divider ref={targetArray[3]} />
       <Step4 show={showedPageNumbers.includes(4)} />
+      <Divider ref={targetArray[4]} />
       <Step5 show={showedPageNumbers.includes(5)} />
     </>
   );
