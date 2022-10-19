743347d26236bb6c348ea53c564c8627721fe217
yukong <kyb062666@gmail.com>
2022-10-19 21:05:59 +0900

    refactor: 사용하지 않는 에러코드 제거

--- backend/src/main/java/com/woowacourse/momo/storage/exception/GroupImageErrorCode.java
+++ backend/src/main/java/com/woowacourse/momo/storage/exception/GroupImageErrorCode.java
@@ -11,8 +11,6 @@ public enum GroupImageErrorCode implements ErrorCode {
 
     MEMBER_IS_NOT_HOST(400, "GROUP_IMAGE_001", "모임의 주최자가 아닙니다."),
 
-    MULTIPART_FILE_NAME_IS_NULL(400, "GROUP_IMAGE_002", "이미지 파일의 이름이 null 입니다."),
-
     RESPONSE_IS_4XX(400, "GROUP_IMAGE_003", "이미지 서버에서 4XX 에러가 발생하였습니다."),
     RESPONSE_IS_5XX(400, "GROUP_IMAGE_004", "이미지 서버에서 5XX 에러가 발생하였습니다."),
     RESPONSE_IS_NULL(400, "GROUP_IMAGE_004", "이미지 서버의 응답값이 null 입니다."),
