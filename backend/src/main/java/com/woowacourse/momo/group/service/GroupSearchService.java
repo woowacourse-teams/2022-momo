4db0691c623b9a8e7ef97e8b70ce1c83bc5d2a6e
yukong <kyb062666@gmail.com>
2022-10-19 15:39:21 +0900

    refactor: 메서드 내 파라미터 및 변수명 수정

--- backend/src/main/java/com/woowacourse/momo/group/service/GroupSearchService.java
+++ backend/src/main/java/com/woowacourse/momo/group/service/GroupSearchService.java
@@ -112,12 +112,12 @@ public class GroupSearchService {
         return PageRequest.of(request.getPage(), DEFAULT_PAGE_SIZE);
     }
 
-    private List<GroupSummaryRepositoryResponse> convertImageUrl(Page<GroupSummaryRepositoryResponse> groups) {
-        for (GroupSummaryRepositoryResponse group : groups.getContent()) {
-            String imageName = group.getImageName();
-            String imageUrl = imageProvider.generateGroupImageUrl(imageName, group.getCategory().isDefaultImage(imageName));
-            group.setImageName(imageUrl);
+    private List<GroupSummaryRepositoryResponse> convertImageUrl(Page<GroupSummaryRepositoryResponse> repositoryResponses) {
+        for (GroupSummaryRepositoryResponse repositoryResponse : repositoryResponses.getContent()) {
+            String imageName = repositoryResponse.getImageName();
+            String imageUrl = imageProvider.generateGroupImageUrl(imageName, repositoryResponse.getCategory().isDefaultImage(imageName));
+            repositoryResponse.setImageName(imageUrl);
         }
-        return groups.getContent();
+        return repositoryResponses.getContent();
     }
 }
