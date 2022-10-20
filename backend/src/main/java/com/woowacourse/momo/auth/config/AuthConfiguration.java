42b43f86bb1b3191259531fd7c6c16c0ce65a3c8
yukong <kyb062666@gmail.com>
2022-10-20 19:57:08 +0900

    refactor: 모든 리소스 접근 허용

--- backend/src/main/java/com/woowacourse/momo/auth/config/AuthConfiguration.java
+++ backend/src/main/java/com/woowacourse/momo/auth/config/AuthConfiguration.java
@@ -31,7 +31,7 @@ public class AuthConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/docs/index.html")
+        registry.addResourceHandler("/*")
                 .addResourceLocations("classpath:/static/");
     }
 
