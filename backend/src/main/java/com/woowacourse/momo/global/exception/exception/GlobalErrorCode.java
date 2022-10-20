e840427011c0547009d6c5e42ac2e3be6c93a15a
sinb57 <sinb57@naver.com>
2022-10-20 11:14:34 +0900

    refactor: NOT_SUPPORTED_METHOD_ERROR 상태코드 수정

--- backend/src/main/java/com/woowacourse/momo/global/exception/exception/GlobalErrorCode.java
+++ backend/src/main/java/com/woowacourse/momo/global/exception/exception/GlobalErrorCode.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum GlobalErrorCode implements ErrorCode {
 
     NOT_SUPPORTED_URI_ERROR(404, "NOT_SUPPORTED_URI", "지원하지 않는 URI 요청입니다."),
-    NOT_SUPPORTED_METHOD_ERROR(404, "NOT_SUPPORTED_METHOD", "지원하지 않는 Method 요청입니다."),
+    NOT_SUPPORTED_METHOD_ERROR(405, "NOT_SUPPORTED_METHOD", "지원하지 않는 Method 요청입니다."),
     VALIDATION_ERROR(400, "VALIDATION_001", "잘못된 요청입니다."),
     INTERNAL_SERVER_ERROR(500, "SERVER_001", "내부 서버 오류입니다."),
     ;
