cf4e99280a96aaec5700bb83aa3e45253936d731
이아현 <lah1203@naver.com>
2022-10-30 16:37:35 +0900

    chore: 공통 컴포넌트를 위한 스토리북 설정 추가

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -8,5 +10,42 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: 'webpack5',
+  },
+  webpackFinal: async config => {
+    config.module.rules = config.module.rules.map(rule => {
+      if (!rule.test.test('.svg')) {
+        return rule;
+      }
+
+      const newRule = {
+        ...rule,
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
+      };
+
+      return newRule;
+    });
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack', 'file-loader'],
+    });
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      apis: path.resolve(__dirname, '../src/apis'),
+      assets: path.resolve(__dirname, '../src/assets'),
+      components: path.resolve(__dirname, '../src/components'),
+      pages: path.resolve(__dirname, '../src/pages'),
+      layouts: path.resolve(__dirname, '../src/layouts'),
+      hooks: path.resolve(__dirname, '../src/hooks'),
+      styles: path.resolve(__dirname, '../src/styles'),
+      types: path.resolve(__dirname, '../src/types'),
+      store: path.resolve(__dirname, '../src/store'),
+      utils: path.resolve(__dirname, '../src/utils'),
+      constants: path.resolve(__dirname, '../src/constants'),
+      mocks: path.resolve(__dirname, '../src/mocks'),
+    };
+
+    return config;
   },
 };
--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,3 +1,34 @@
+import { ThemeProvider } from '@emotion/react';
+import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { MemoryRouter } from 'react-router-dom';
+
+import theme from 'styles/theme';
+import GlobalStyle from 'styles/global';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export const decorators = [
+  Story => (
+    <ThemeProvider theme={theme}>
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <GlobalStyle />
+            <Story />
+          </MemoryRouter>
+        </QueryClientProvider>
+      </RecoilRoot>
+    </ThemeProvider>
+  ),
+];
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
