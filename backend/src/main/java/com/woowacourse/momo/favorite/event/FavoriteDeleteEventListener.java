7501c8f19ab7f131cf764fa4152762e17231ff27
SeongWon Oh <osw970325@naver.com>
2023-01-04 22:14:48 +0900

    [BE] 찜하기 데이터 삭제 관련 이벤트 처리방식을 비동기로 변경 (#549)
    * style: 불필요한 Import 제거
    
    * feat: 기존 동기 처리의 이벤트를 비동기로 변경
    
    * feat: 이벤트의 바인딩 시점을 publish 트랜잭션의 커밋 이후로 지정
    
    * feat: 비동기 처리를 스레드 풀의 스레드를 이용해 처리하도록 개선

--- backend/src/main/java/com/woowacourse/momo/favorite/event/FavoriteDeleteEventListener.java
+++ backend/src/main/java/com/woowacourse/momo/favorite/event/FavoriteDeleteEventListener.java
@@ -1,7 +1,9 @@
 package com.woowacourse.momo.favorite.event;
 
-import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,17 +12,20 @@ import com.woowacourse.momo.group.event.GroupDeleteEvent;
 import com.woowacourse.momo.member.event.MemberDeleteEvent;
 
 @RequiredArgsConstructor
+@Transactional
 @Component
 public class FavoriteDeleteEventListener {
 
     private final FavoriteRepository favoriteRepository;
 
-    @EventListener
+    @TransactionalEventListener
+    @Async
     public void deleteGroup(GroupDeleteEvent event) {
         favoriteRepository.deleteAllByGroupId(event.getId());
     }
 
-    @EventListener
+    @TransactionalEventListener
+    @Async
     public void deleteMember(MemberDeleteEvent event) {
         favoriteRepository.deleteAllByMemberId(event.getId());
     }
---
+++ backend/src/main/java/com/woowacourse/momo/global/config/AsyncConfiguration.java
@@ -0,0 +1,22 @@
+package com.woowacourse.momo.global.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfiguration {
+
+    @Bean
+    public Executor asyncThreadTaskExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setCorePoolSize(4);
+        threadPoolTaskExecutor.setMaxPoolSize(4);
+        threadPoolTaskExecutor.setThreadNamePrefix("momo-pool");
+        return threadPoolTaskExecutor;
+    }
+}
