741d01440351514c99b990eeb56fbeac45b0b5dd
yukong <92148749+kyukong@users.noreply.github.com>
2023-03-21 12:26:38 +0900

    [BE] 이미지 서버 정보 변경 (#559)
    * chore: image server 정보 변경
    
    * test: 테스트 검증 시 이미지 도메인 제외

--- backend/src/test/java/com/woowacourse/momo/group/service/GroupSearchServiceTest.java
+++ backend/src/test/java/com/woowacourse/momo/group/service/GroupSearchServiceTest.java
@@ -127,7 +127,7 @@ class GroupSearchServiceTest {
 
         assertThat(actual.getGroups()).hasSize(1);
         assertThat(actual.getGroups().get(0).getImageUrl())
-                .isEqualTo("https://image.moyeora.site/group/default/thumbnail_study.jpg");
+            .endsWith("/group/default/thumbnail_study.jpg");
     }
 
     @DisplayName("키워드를 포함하는 이름의 모임을 조회한다")
